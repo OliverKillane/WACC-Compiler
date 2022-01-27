5c7116b2aa9169f0f682acdcb60020e06b3e9ed6
Killane, Oliver <oliver.killane20@imperial.ac.uk>
2022-01-27 23:15:37 +0000

    Completed basic setup of project structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![warn(missing_docs)]
+#![doc(html_logo_url = "https://i.imgur.com/cBcRWvM.png")]
+#![feature(box_syntax)]
+//! The group 33 WACC Compiler project.
+//!
+//! Lovingly developed by:
+//! - Jordan Hall
+//! - Bartłomiej Cieślar
+//! - Panayiotis Gavriil
+//! - Oliver Killane
+
+mod backend;
+mod frontend;
+mod intermediate;
+
+/// Compiler main entry.
+/// - Processes command line arguments controlling compiler behaviour.
+/// - Halts and reports failures through returning exit codes.
 fn main() {
-    println!("Hello, world!");
+    todo!()
 }
