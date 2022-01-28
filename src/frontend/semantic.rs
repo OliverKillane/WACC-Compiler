86b0de7dff1fb95362f1f888b3fe674eb4d51b21
Oliver Killane <oliver.killane20@imperial.ac.uk>
2022-01-28 13:53:13 +0000

    Created chained local symbol table for translation of string identifiers to (renamed identifier, span of original definition) [ ok220 ]

--- src/frontend/semantic.rs
+++ src/frontend/semantic.rs
@@ -24,11 +24,65 @@ type FunctionSymbolTable = HashMap<String, (Type, Vec<Type>)>;
 type VariableSymbolTable = HashMap<usize, Type>;
 
 /// A chainable local symbol table, used to translate the current (and parent)
-/// scope variables to integers for use in the flat variable symbol table.
-struct LocalSymbolTable<'a, 'b>(
-    HashMap<&'a str, usize>,
-    Option<&'b LocalSymbolTable<'a, 'b>>,
-);
+/// scope variables to integers for use in the flat variable symbol table and the definition of the variable.
+struct LocalSymbolTable<'a, 'b> {
+    current: HashMap<&'a str, (usize, &'a str)>,
+    parent: Option<&'b LocalSymbolTable<'a, 'b>>,
+}
+
+impl<'a, 'b> LocalSymbolTable<'a, 'b> {
+    /// Create a new root scope (no parent)
+    fn new_root() -> Self {
+        LocalSymbolTable {
+            current: HashMap::new(),
+            parent: None,
+        }
+    }
+
+    /// Create new child scope local symbol table
+    fn new_child(parent: &'b LocalSymbolTable<'a, 'b>) -> Self {
+        LocalSymbolTable {
+            current: HashMap::new(),
+            parent: Some(&parent),
+        }
+    }
+
+    /// Checks if a variable is in the current scope level. If one is, then
+    /// the span of the definition of the first is returned.
+    fn in_scope(&self, ident: &'a str) -> Option<&'a str> {
+        match self.current.get(ident) {
+            Some((_, def)) => Some(def),
+            None => None,
+        }
+    }
+
+    /// Add a variable to the current scope. Assumes the variable is not already
+    /// in the current scope, and panics in that case.
+    ///
+    /// Should only be called once it has been established the variable is not
+    /// yet defined.
+    fn add_var(&mut self, ident: &'a str, id: usize, span: &'a str) {
+        if let Some(_) = self.current.insert(ident, (id, span)) {
+            panic!("Repeat add of variable to local symbol table.")
+        }
+    }
+
+    /// Get the renamed identifier and definition span of a variable.
+    /// If the variable is undefined None is returned.
+    fn get_var(&self, ident: &'a str) -> Option<(usize, &'a str)> {
+        let mut symb = self;
+        loop {
+            if let Some((id, span)) = symb.current.get(ident) {
+                break Some((*id, span));
+            }
+
+            match symb.parent {
+                Some(parent) => symb = parent,
+                None => break None,
+            }
+        }
+    }
+}
 
 /// Holds the type constraints for an expression (concrete types, pointer type).
 /// ```text
@@ -54,7 +108,7 @@ pub struct TypeConstraint(Vec<Type>, bool);
 
 /// Contains a single error within an expression.
 #[derive(Debug, PartialEq, Eq)]
-pub enum ExpressionError<'a> {
+pub enum ExprErrType<'a> {
     /// Invalid type, including when no type inference is possible.
     /// ```text
     /// bool a = 9 + 9
@@ -98,6 +152,37 @@ pub enum ExpressionError<'a> {
     /// ExpressionError::UndefinedVar("b")
     /// ```
     UndefinedVar(&'a str),
+}
+
+/// Determines the expression error wrapping of an expression.
+#[derive(Debug, PartialEq, Eq)]
+pub enum ExpressionError<'a> {
+    /// When the entire expression is erroneous.
+    /// ```text
+    /// int a = bool
+    /// ```
+    /// ```
+    /// ExpressionError::Is(ExprErrType::InvalidType("bool", TypeConstraint(vec![Type::Int], false), Some(Type::Bool)))
+    /// ```
+    /// ```text
+    /// char a = - true
+    /// ```
+    /// ```
+    /// ExpressionError::Is(ExprErrType::InvalidType("- true", TypeConstraint(vec![Type::Char], false), None))
+    /// ```
+    Is(ExprErrType<'a>),
+
+    /// The expression contains some errors, not all of the expression is erroneous
+    /// ```text
+    /// int a = (1 + true) + (3 * 'a')
+    /// ```
+    /// ```
+    /// ExpressionError::Contains(vec![
+    ///     ExprErrType::InvalidType("true", TypeConstraint(vec![Type::Int], false), Some(Type::Bool)),
+    ///     ExprErrType::InvalidType("'a'", TypeConstraint(vec![Type::Int], false), Some(Type::Char)),
+    /// ])
+    /// ```
+    Contains(Vec<ExprErrType<'a>>),
 }
 
 pub struct SemanticErrorSummary<'a>(Vec<WrapSpan<'a, SemanticError<'a>>>);
@@ -151,7 +236,7 @@ pub enum SemanticError<'a> {
     ///     ]
     /// )
     /// ```
-    AssignmentMismatch(&'a str, Vec<ExpressionError<'a>>),
+    AssignmentMismatch(&'a str, ExpressionError<'a>),
 
     /// Function of same name is defined twice
     /// (Function name, span of original definition)
@@ -200,7 +285,7 @@ pub enum SemanticError<'a> {
     /// ```
     /// FunctionParametersMismatch("function", vec![Type::Int, Type::Char], vec![Type::Bool, Type::Bool])
     /// ```
-    FunctionParametersMismatch(&'a str, Vec<(&'a str, Vec<ExpressionError<'a>>)>),
+    FunctionParametersMismatch(&'a str, Vec<(&'a str, ExpressionError<'a>)>),
 
     /// Function return does not match function return type.
     /// (Function name, expected type, found type)
@@ -216,7 +301,7 @@ pub enum SemanticError<'a> {
     /// ```
     /// FunctionReturnMismatch("function", Type::Bool, Type::Int)
     /// ```
-    FunctionReturnMismatch(&'a str, Vec<ExpressionError<'a>>),
+    FunctionReturnMismatch(&'a str, ExpressionError<'a>),
 
     /// Function has a control flow that does not end in return or exit.
     /// (Function name)
@@ -320,7 +405,7 @@ pub enum SemanticError<'a> {
     /// ```
     /// FreeStatementMismatch("a", vec![ExpressionError("a", )])
     /// ```
-    FreeStatementMismatch(&'a str, Vec<ExpressionError<'a>>),
+    FreeStatementMismatch(&'a str, ExpressionError<'a>),
 
     /// Exit Statement Invalid (wrong type)
     /// (span of expression, type found)
@@ -332,12 +417,12 @@ pub enum SemanticError<'a> {
     /// ```
     /// ExitStatementMismatch("")
     /// ```
-    ExitStatementMismatch(&'a str, Vec<ExpressionError<'a>>),
+    ExitStatementMismatch(&'a str, ExpressionError<'a>),
 
     /// Print/Println Statement invalid (wrong type).
     /// Currently all types can be printed, this is here for extensibility.
     /// (span of print/println statement, type found)
-    PrintStatementMisMatch(&'a str, Vec<ExpressionError<'a>>),
+    PrintStatementMisMatch(&'a str, ExpressionError<'a>),
 
     /// Return statement outside of a function
     /// Only occurs in the main code block.
@@ -407,7 +492,6 @@ pub fn semantic_analysis<'a>(
     todo!()
 }
 
-
 /*
   Todo:
   1. Remove references to ExpressionError (the old version)
@@ -418,3 +502,141 @@ pub fn semantic_analysis<'a>(
   6. analyse_scope
   7. semantic_analysis
 */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Local Symbol Table Tests
+    #[test]
+    fn local_symb_can_add_and_find_vars_in_current_scope() {
+        let mut symb_tab = LocalSymbolTable::new_root();
+
+        symb_tab.add_var("var1", 0, "int var1 = 9");
+        symb_tab.add_var("var2", 1, "bool var2 = true");
+
+        assert_eq!(symb_tab.in_scope("var1"), Some("int var1 = 9"));
+        assert_eq!(symb_tab.in_scope("var2"), Some("bool var2 = true"));
+
+        assert_eq!(symb_tab.get_var("var1"), Some((0, "int var1 = 9")));
+        assert_eq!(symb_tab.get_var("var2"), Some((1, "bool var2 = true")));
+
+        assert_eq!(symb_tab.current.len(), 2);
+    }
+
+    #[test]
+    fn local_symb_can_add_variables_to_child() {
+        let symb_parent = LocalSymbolTable::new_root();
+
+        let mut symb_child = LocalSymbolTable::new_child(&symb_parent);
+
+        symb_child.add_var("var2", 1, "bool var2 = true");
+        symb_child.add_var("var1", 0, "int var1 = 9");
+
+        assert_eq!(symb_child.in_scope("var1"), Some("int var1 = 9"));
+        assert_eq!(symb_child.in_scope("var2"), Some("bool var2 = true"));
+
+        assert_eq!(symb_child.get_var("var1"), Some((0, "int var1 = 9")));
+        assert_eq!(symb_child.get_var("var2"), Some((1, "bool var2 = true")));
+
+        assert_eq!(symb_child.current.len(), 2);
+        assert_eq!(symb_parent.current.len(), 0);
+    }
+
+    #[test]
+    fn local_symb_can_find_variables_in_parent() {
+        let mut symb_parent = LocalSymbolTable::new_root();
+
+        symb_parent.add_var("var1", 0, "int var1 = 9");
+        symb_parent.add_var("var2", 1, "bool var2 = true");
+
+        assert_eq!(symb_parent.in_scope("var1"), Some("int var1 = 9"));
+        assert_eq!(symb_parent.in_scope("var2"), Some("bool var2 = true"));
+
+        assert_eq!(symb_parent.get_var("var1"), Some((0, "int var1 = 9")));
+        assert_eq!(symb_parent.get_var("var2"), Some((1, "bool var2 = true")));
+
+        assert_eq!(symb_parent.current.len(), 2);
+
+        let mut symb_child = LocalSymbolTable::new_child(&symb_parent);
+
+        assert_eq!(symb_child.get_var("var1"), Some((0, "int var1 = 9")));
+        assert_eq!(symb_child.get_var("var2"), Some((1, "bool var2 = true")));
+    }
+
+    #[test]
+    fn local_symb_variables_can_shadow() {
+        let mut symb_parent = LocalSymbolTable::new_root();
+
+        symb_parent.add_var("var1", 0, "int var1 = 9");
+        symb_parent.add_var("var2", 1, "bool var2 = true");
+
+        assert_eq!(symb_parent.in_scope("var1"), Some("int var1 = 9"));
+        assert_eq!(symb_parent.in_scope("var2"), Some("bool var2 = true"));
+
+        assert_eq!(symb_parent.get_var("var1"), Some((0, "int var1 = 9")));
+        assert_eq!(symb_parent.get_var("var2"), Some((1, "bool var2 = true")));
+
+        assert_eq!(symb_parent.current.len(), 2);
+
+        let mut symb_child = LocalSymbolTable {
+            current: HashMap::new(),
+            parent: Some(&symb_parent),
+        };
+
+        assert_eq!(symb_child.get_var("var1"), Some((0, "int var1 = 9")));
+        assert_eq!(symb_child.get_var("var2"), Some((1, "bool var2 = true")));
+
+        symb_child.add_var("var1", 2, "char var2 = 'a'");
+        symb_child.add_var("var2", 3, "string var1 = \"hello\"");
+
+        assert_eq!(symb_child.get_var("var1"), Some((2, "char var2 = 'a'")));
+        assert_eq!(
+            symb_child.get_var("var2"),
+            Some((3, "string var1 = \"hello\""))
+        );
+    }
+
+    #[test]
+    fn local_symb_sibling_scopes_disjoint() {
+        let mut symb_parent = LocalSymbolTable::new_root();
+
+        symb_parent.add_var("var1", 0, "int var1 = 9");
+        symb_parent.add_var("var2", 1, "bool var2 = true");
+
+        {
+            let mut symb_child1 = LocalSymbolTable::new_child(&symb_parent);
+
+            assert_eq!(symb_child1.get_var("var1"), Some((0, "int var1 = 9")));
+            assert_eq!(symb_child1.get_var("var2"), Some((1, "bool var2 = true")));
+            assert_eq!(symb_child1.get_var("var3"), None);
+
+            assert_eq!(symb_parent.current.len(), 2);
+
+            symb_child1.add_var("var3", 2, "char var3 = 'b'");
+
+            assert_eq!(symb_child1.get_var("var3"), Some((2, "char var3 = 'b'")));
+        }
+
+        {
+            let symb_child2 = LocalSymbolTable::new_child(&symb_parent);
+
+            assert_eq!(symb_child2.get_var("var1"), Some((0, "int var1 = 9")));
+            assert_eq!(symb_child2.get_var("var2"), Some((1, "bool var2 = true")));
+
+            assert_eq!(symb_parent.current.len(), 2);
+
+            assert_eq!(symb_child2.get_var("var3"), None);
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn local_symb_fails_to_redefine_in_same_scope() {
+        let mut symb_tab = LocalSymbolTable::new_root();
+
+        symb_tab.add_var("var1", 0, "int var1 = 9");
+
+        symb_tab.add_var("var1", 1, "bool var2 = true");
+    }
+}
