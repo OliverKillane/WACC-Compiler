d976c53c2f357626251e377a2696f20f268db4f8
Oliver Killane <oliver.killane20@imperial.ac.uk>
2022-01-30 03:48:45 +0000

    Added tests and fixed a minor bug with type index reducing [ ok200 ]

--- src/frontend/ast.rs
+++ src/frontend/ast.rs
@@ -169,20 +169,45 @@ impl Type {
     ///   (e.g Array(Array(int),1),1) == Array(int, 2) )
     pub fn coalesce(&self, other: &Self) -> bool {
         match (self, other) {
-            (Type::Any, _) => true,
-            (_, Type::Any) => true,
-            (Type::Pair(box a1, box a2), Type::Pair(box b1, box b2)) => a1 == b1 && a2 == b2,
+            (Type::Any | Type::Generic, _) => true,
+            (_, Type::Any | Type::Generic) => true,
+            (Type::Pair(box a1, box a2), Type::Pair(box b1, box b2)) => {
+                a1.coalesce(b1) && a2.coalesce(b2)
+            }
             (Type::Array(box a, dim_a), Type::Array(box b, dim_b)) => {
                 if dim_a == dim_b {
-                    a == b
+                    a.coalesce(b)
                 } else if dim_a > dim_b {
-                    &Type::Array(box a.clone(), dim_a - dim_b) == b
+                    (&Type::Array(box a.clone(), dim_a - dim_b)).coalesce(b)
                 } else {
-                    a == &Type::Array(box b.clone(), dim_b - dim_a)
+                    a.coalesce(&Type::Array(box b.clone(), dim_b - dim_a))
                 }
             }
             (ta, tb) => ta == tb,
         }
+    }
+
+    /// Reduce the indexing depth of a type.
+    /// ```
+    /// assert_eq!(Type::Array(Type::Int, 4).reduce_index_depth(4), Type::Int)
+    /// ```
+    pub fn reduce_index_depth(self, levels: usize) -> Option<Self> {
+        if let Type::Array(box t, dim) = self {
+            if levels == dim {
+                Some(t)
+            } else if levels < dim {
+                Some(Type::Array(box t, dim - levels))
+            } else {
+                t.reduce_index_depth(levels - dim)
+            }
+        } else {
+            None
+        }
+    }
+
+    /// Index index depth of a type
+    pub fn increase_index_depth(self, levels: usize) -> Self {
+        Type::Array(box self, levels)
     }
 }
 
@@ -663,7 +688,7 @@ mod test {
 
     // Tests for the implementation of equality on types
     #[test]
-    fn type_eq_primitive_types_are_equal() {
+    fn type_coalesce_primitive_types_are_equal() {
         assert!(Type::Int.coalesce(&Type::Int));
         assert!(Type::Char.coalesce(&Type::Char));
         assert!(Type::String.coalesce(&Type::String));
@@ -671,7 +696,7 @@ mod test {
     }
 
     #[test]
-    fn type_eq_any_type_matches_anything() {
+    fn type_coalesce_any_type_matches_anything() {
         assert!(Type::Any.coalesce(&Type::Int));
         assert!(Type::Int.coalesce(&Type::Any,));
         assert!(Type::Any.coalesce(&Type::Char));
@@ -695,12 +720,12 @@ mod test {
     }
 
     #[test]
-    fn type_eq_generic_type_matches_itself() {
+    fn type_coalesce_generic_type_matches_itself() {
         assert!(Type::Generic.coalesce(&Type::Generic));
     }
 
     #[test]
-    fn type_eq_differently_nested_array_types_are_equal() {
+    fn type_coalesce_differently_nested_array_types_are_equal() {
         assert!(Type::Array(box Type::Array(box Type::Int, 2), 3)
             .coalesce(&Type::Array(box Type::Int, 5)));
         assert!(Type::Array(box Type::Array(box Type::Any, 2), 3)
@@ -718,26 +743,89 @@ mod test {
     }
 
     #[test]
-    fn type_eq_differentiates_between_primitive_and_generic_types() {
+    fn type_coalesce_differentiates_between_primitive_types() {
         assert_eq!(Type::Int.coalesce(&Type::Bool), false);
         assert_eq!(Type::Int.coalesce(&Type::Char), false);
         assert_eq!(Type::Int.coalesce(&Type::String), false);
-        assert_eq!(Type::Int.coalesce(&Type::Generic), false);
         assert_eq!(Type::Bool.coalesce(&Type::Int), false);
         assert_eq!(Type::Bool.coalesce(&Type::Char), false);
         assert_eq!(Type::Bool.coalesce(&Type::String), false);
-        assert_eq!(Type::Bool.coalesce(&Type::Generic), false);
         assert_eq!(Type::Char.coalesce(&Type::Int), false);
         assert_eq!(Type::Char.coalesce(&Type::Bool), false);
         assert_eq!(Type::Char.coalesce(&Type::String), false);
-        assert_eq!(Type::Char.coalesce(&Type::Generic), false);
         assert_eq!(Type::String.coalesce(&Type::Int), false);
         assert_eq!(Type::String.coalesce(&Type::Bool), false);
         assert_eq!(Type::String.coalesce(&Type::Char), false);
-        assert_eq!(Type::String.coalesce(&Type::Generic), false);
-        assert_eq!(Type::Generic.coalesce(&Type::Int), false);
-        assert_eq!(Type::Generic.coalesce(&Type::Bool), false);
-        assert_eq!(Type::Generic.coalesce(&Type::Char), false);
-        assert_eq!(Type::Generic.coalesce(&Type::String), false);
+    }
+
+    #[test]
+    fn type_reduce_index_reduces() {
+        assert_eq!(
+            Type::Array(box Type::Int, 3).reduce_index_depth(3),
+            Some(Type::Int)
+        );
+        assert_eq!(
+            Type::Array(box Type::Array(box Type::Int, 2), 3).reduce_index_depth(5),
+            Some(Type::Int)
+        );
+        assert_eq!(
+            Type::Array(box Type::Array(box Type::Int, 2), 3).reduce_index_depth(4),
+            Some(Type::Array(box Type::Int, 1))
+        );
+        assert_eq!(
+            Type::Array(
+                box Type::Array(
+                    box Type::Array(box Type::Array(box Type::Array(box Type::Any, 1), 1), 1),
+                    1
+                ),
+                1
+            )
+            .reduce_index_depth(4),
+            Some(Type::Array(box Type::Any, 1))
+        );
+    }
+
+    #[test]
+    fn type_reduce_index_throw_none_if_impossible() {
+        assert_eq!(
+            Type::Array(box Type::Int, 3).reduce_index_depth(3),
+            Some(Type::Int)
+        );
+        assert_eq!(
+            Type::Array(box Type::Array(box Type::Int, 2), 3).reduce_index_depth(7),
+            None
+        );
+        assert_eq!(
+            Type::Array(box Type::Array(box Type::Int, 2), 3).reduce_index_depth(10),
+            None
+        );
+        assert_eq!(
+            Type::Array(
+                box Type::Array(
+                    box Type::Array(box Type::Array(box Type::Array(box Type::Any, 1), 1), 1),
+                    1
+                ),
+                1
+            )
+            .reduce_index_depth(7),
+            None
+        );
+        assert_eq!(Type::Int.reduce_index_depth(1), None);
+    }
+
+    #[test]
+    fn type_increase_index_increases_index() {
+        assert_eq!(
+            Type::Int.increase_index_depth(4),
+            Type::Array(box Type::Int, 4)
+        );
+        assert_eq!(
+            Type::Char.increase_index_depth(2),
+            Type::Array(box Type::Char, 2)
+        );
+        assert_eq!(
+            Type::Any.increase_index_depth(3),
+            Type::Array(box Type::Any, 3)
+        );
     }
 }
