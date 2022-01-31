4b9f3303eaa509d01fe077dee149bb1236a0a829
Oliver Killane <oliver.killane20@imperial.ac.uk>
2022-01-31 18:12:13 +0000

    Greatly simplified semantic errors [ ok220 ]

--- src/frontend/semantic/semantic_errors.rs
+++ src/frontend/semantic/semantic_errors.rs
@@ -10,10 +10,14 @@ use crate::frontend::semantic::type_constraints::*;
 
 /// Contains all errors which can occur in expressions
 #[derive(Debug, PartialEq, Eq)]
-pub enum ExprError<'a> {
+pub enum SemanticError<'a> {
+    /// An undefined variable is used.
+    /// (span of variable in assignment)
+    UndefinedVariableAssignment(&'a str),
+
     /// An undefined variable is used.
     /// (span of variable use)
-    UndefinedVariable(&'a str),
+    UndefinedVariableUse(&'a str),
 
     /// Invalid Index (A non-integer was used as an index)
     /// (span of indexing expression)
@@ -38,193 +42,54 @@ pub enum ExprError<'a> {
     /// Invalid unary operator application
     /// (span of operator, span inner expr, possible types, possible unops, found type, found unop)
     InvalidUnOp(&'a str, &'a str, Vec<Type>, Vec<&'static UnOp>, Type, UnOp),
-}
-
-/// Semantic errors with their spans
-pub type SemanticErrorSummary<'a> = Vec<WrapSpan<'a, SemanticError<'a>>>;
-
-/// Semantic errors and their types.
-/// Each variant corresponds to an error for which we want a distinct error message.
-///
-/// Many errors are the result of expression type mismatches, by having variants
-/// to cover multiple instances of this (e.g return, function arguments, etc)
-/// more detailed error messages can be produced.
-#[derive(PartialEq, Eq, Debug)]
-pub enum SemanticError<'a> {
-    /// Function used is undefined.
-    /// (function name - reference to source code)
-    /// ```text
-    /// begin
-    ///     int a = call undefined_function(4)
-    /// end
-    /// ```
-    /// ```
-    /// UndefinedFunction("undefined_function")
-    /// ```
-    UndefinedFunction(&'a str),
-
-    /// Variable assigned incorrectly, either undefined assignment or
-    /// expression errors.
-    /// (Variable name - reference to source code)
-    /// ```text
-    /// begin
-    ///     a = 3
-    /// end
-    /// ```
-    /// ```
-    /// UndefinedVariableAssignment(Some("a"), vec![])
-    /// ```
-    /// ```text
-    /// begin
-    ///     a = 3 + true
-    /// end
-    /// ```
-    /// ```
-    /// UndefinedVariableAssignment(Some("a"), vec![ExprError::InvalidBinOpRight("true", vec![Type::Int], vec![], Type::Bool, BinOp::Add)])
-    /// ```
-    InvalidVariableAssignment(Option<&'a str>, Vec<ExprError<'a>>),
-
-    /// Function of same name is defined twice
-    /// (Function name, span of original definition)
-    /// ```text
-    /// begin
-    ///     int function(int a) is
-    ///         return 3
-    ///     end
-    ///
-    ///     int function(int b, bool c) is
-    ///         return 3
-    ///     end
-    ///
-    ///     skip
-    /// end
-    /// ```
-    /// ```
-    /// RepeatDefinitionFunction("function", "int function(int a = 3)")
-    /// ```
-    RepeatDefinitionFunction(&'a str, &'a str),
 
     /// Variable redefined in the same scope.
     /// (Variable name, original definition)
-    /// ```text
-    /// begin
-    ///     int a = 3 ;
-    ///     bool a = true
-    /// end
-    /// ```
-    /// ```
-    /// RepeatDefinitionVariable("a", "int a = 3")
-    /// ```
     RepeatDefinitionVariable(&'a str, &'a str),
+
+    /// An undefined function was called
+    /// (span of function name in call)
+    UndefinedFunction(&'a str),
+
+    /// Function of same name is defined twice
+    /// (Function name, span of original definition)
+    RepeatDefinitionFunction(&'a str, &'a str),
 
     /// The number of arguments provided is too small or large
     /// (function name, expected length, found length)
-    /// ```text
-    /// begin
-    ///     int fun1(int a, int b) is
-    ///         return 9
-    ///     end
-    ///     
-    ///     int a = call fun1(3)
-    /// end
-    /// ```
-    /// ```
-    /// FunctionParametersLengthMismatch("fun1", 2, 1)
-    /// ```
     FunctionParametersLengthMismatch(&'a str, usize, usize),
 
-    /// Function argument types do not match parameters.
-    /// (Function name, [ (param name, expression problems) ] )
-    /// ```text
-    /// begin
-    ///     int function(int a, char b) is
-    ///         return 3
-    ///     end
-    ///
-    ///     int a = call function(true, true)
-    /// end
-    /// ```
-    InvalidFunctionArguments(&'a str, Vec<(&'a str, Vec<ExprError<'a>>)>),
+    /// Invalid function argument
+    /// (span of argument, type expected, type found)
+    FunctionArgumentTypeInvalid(&'a str, Type, Type),
 
-    /// Function return does not match function return type.
-    /// (Function name, Result(type, expression's errors), expected type)
-    InvalidFunctionReturn(&'a str, Result<Type, Vec<ExprError<'a>>>, Type),
+    /// Function return type is incorrect
+    /// (span of return expression, type expected, type found)
+    InvalidFunctionReturn(&'a str, Type, Type),
 
     /// Function has a control flow that does not end in return or exit.
     /// (Function name)
-    /// ```text
-    /// begin
-    ///     bool function(int a) is
-    ///         int i = 0 ;
-    ///         print i
-    ///     end
-    ///
-    ///     skip
-    /// end
-    /// ```
-    /// ```
-    /// FunctionNoReturnOrExit("function")
-    /// ```
-    /// or
-    /// ```text
-    /// begin
-    ///     bool function(int a) is
-    ///         if ( b ) then
-    ///             return b
-    ///         else
-    ///             skip
-    ///         fi
-    ///     end
-    ///
-    ///     skip
-    /// end
-    /// ```
-    /// ```
-    /// FunctionNoReturnOrExit("function")
-    /// ```
     FunctionNoReturnOrExit(&'a str),
 
     /// Read statement invalid (wrong type)
     /// (variable/lvalue identifier, Result(type found, expression had errors))
-    /// ```text
-    /// begin
-    ///     int[] a = [9,3,4] ;
-    ///     read a
-    /// end
-    /// ```
-    ReadStatementMismatch(&'a str, Result<Type, Vec<ExprError<'a>>>),
+    ReadStatementMismatch(&'a str, Result<Type, Vec<SemanticError<'a>>>),
 
     /// Free statement invalid (wrong type)
     /// (span of expression, Result(type found, expression had errors))
-    /// ```text
-    /// begin
-    ///     int a = 4 ;
-    ///     free a
-    /// end
-    /// ```
-    FreeStatementMismatch(&'a str, Result<Type, Vec<ExprError<'a>>>),
+    FreeStatementMismatch(&'a str, Result<Type, Vec<SemanticError<'a>>>),
 
     /// Exit Statement Invalid (wrong type)
     /// (span of expression, type found)
-    /// ```text
-    /// begin
-    ///     exit (true || false)
-    /// end
-    /// ```
-    ExitStatementMismatch(&'a str, Result<Type, Vec<ExprError<'a>>>),
+    ExitStatementMismatch(&'a str, Result<Type, Vec<SemanticError<'a>>>),
 
     /// Print/Println Statement invalid (wrong type).
     /// Currently all types can be printed, this is here for extensibility.
     /// (span of print/println statement, type found)
-    PrintStatementMisMatch(&'a str, Result<Type, Vec<ExprError<'a>>>),
+    PrintStatementMisMatch(&'a str, Type, Type),
 
-    /// Return statement outside of a function
-    /// Only occurs in the main code block.
-    /// (span of return statement)
-    /// ```text
-    /// begin
-    ///     return 3
-    /// end
-    /// ```
+    /// Return statement outside of a function Only occurs in the main block.
     ReturnStatementMisplaced,
 }
+
+pub type SemanticErrorSummary<'a> = Vec<WrapSpan<'a, SemanticError<'a>>>;
