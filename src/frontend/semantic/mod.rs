be168fb084ba59a71afeef4f8909689365506b39
Oliver Killane <oliver.killane20@imperial.ac.uk>
2022-02-01 15:01:42 +0000

    Updated documentation for semantic module and minor clean of imports & public passthrough of semantic [ ok220 ]

--- src/frontend/semantic/mod.rs
+++ src/frontend/semantic/mod.rs
@@ -1,9 +1,48 @@
-pub mod expression_analysis;
-pub mod function_analysis;
+//! Analyses the ast and flattens naming to produce a variable symbol table.
+//! 
+//! ## Errors
+//! All error types are contained within the [semantic errors](semantic_errors) 
+//! submodule.
+//! 
+//! When any errors occurs the semantic analyser outputs a tuple:
+//! (function definition errors, main body errors, errors per function)
+//! 
+//! Push forward is deeply engrained into the analysis, a single statement or 
+//! expression can contain multiple errors, and every effort is made to 
+//! determine errors in the absence of information due to other errors (e.g 
+//! expression errors in assignments where the variable is undefined).
+//! 
+//! The spans associated with errors are returned, this ensures that the span 
+//! of erroneous code in the source is kept for error printing later.
+//! 
+//! For each statement the errors is given as:
+//! (span of statement, [list of semantic errors (can contain internal spans)])
+//! this is covered in more detail in [semantic errors](semantic_errors).
+//! 
+//! ## Functions
+//! Functions definitions are analysed by the function symbol table generation in 
+//! [symbol tables](symbol_table). However the parameters are analysed in 
+//! [function analysis](function_analysis) which creates the root local scope 
+//! symbol table and makes use of statement block analysis.
+//! 
+//! ## Statement blocks
+//! Parsed in collections of blocks recursively, with termination and return 
+//! type checking. Uses the push forwards strategy extensively.
+//! 
+//! ## Expressions
+//! Expressions are analysed through the operator and type matching in 
+//! [type constraints](type_constraints). This allows for new operators to be 
+//! implemented by simply adding their types to the relevant table.
+//! 
+//! Expressions are also push forwards, and propagate back many errors through 
+//! the expression tree traversal.
+
+mod expression_analysis;
+mod function_analysis;
+mod type_constraints;
+mod statement_analysis;
 pub mod semantic_errors;
-pub mod statement_analysis;
 pub mod symbol_table;
-pub mod type_constraints;
 
 use self::{
     function_analysis::analyse_function,
@@ -12,7 +51,7 @@ use self::{
     symbol_table::{get_fn_symbols, LocalSymbolTable, VariableSymbolTable},
 };
 
-use super::ast::*;
+use super::ast::{Program, WrapSpan};
 
 /// Analyses a program, either returning a flat variable and function symbol table and an ast, or a tuple of error (function defs, main body, function errors)
 pub fn analyse_semantics<'a>(
