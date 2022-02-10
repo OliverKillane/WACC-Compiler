245121e777f83e9c6d50cd8949c3a916ba626d2e
Panayiotis Gavriil <panayiotis.gavriil19@imperial.ac.uk>
2022-02-10 23:13:30 +0000

    Added a couple of tests for Boolean expressions

--- src/frontend/translation/trans_expr.rs
+++ src/frontend/translation/trans_expr.rs
@@ -3,13 +3,14 @@
 use core::num;
 
 use crate::frontend::ast;
+use crate::frontend::semantic::symbol_table::VariableSymbolTable;
 use crate::intermediate::{ArithOp, BoolExpr, BoolOp, Expr::*, NumExpr, NumSize::*, PtrExpr, Type};
 
 use super::super::super::intermediate::Expr as IRExpr;
 use super::super::ast::{Expr, ExprSpan, UnOp, WrapSpan};
 
 /// usize -> &'a str
-pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>) -> IRExpr {
+pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>, var_symb: &VariableSymbolTable) -> IRExpr {
     match ast_expr {
         // pair literal 'null' - maybe define null as PtrExpr in IR?
         Expr::Null => Ptr(PtrExpr::Null),
@@ -34,7 +35,7 @@ pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>) -> IRExpr 
             todo!()
         }
 
-        Expr::UnOp(o, box e) => match (o, translate_expr(e)) {
+        Expr::UnOp(o, box e) => match (o, translate_expr(e, var_symb)) {
             (UnOp::Neg, Bool(b)) => Bool(BoolExpr::Not(box b)),
             (UnOp::Minus, Num(n)) => Num(NumExpr::ArithOp(
                 box NumExpr::Const(DWord, -1),
@@ -45,7 +46,7 @@ pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>) -> IRExpr 
             (UnOp::Ord, Num(n)) => Num(NumExpr::Cast(DWord, box n)),
             _ => panic!("Shouldn't reach this!"),
         },
-        Expr::UnOp(o, v) => match o {
+        Expr::UnOp(o, box v) => match o {
             UnOp::Len => todo!(),
             UnOp::Fst => todo!(),
             UnOp::Snd => todo!(),
@@ -53,7 +54,7 @@ pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>) -> IRExpr 
         },
 
         Expr::BinOp(box e1, o, box e2) => {
-            match (translate_expr(e1), o, translate_expr(e2)) {
+            match (translate_expr(e1, var_symb), o, translate_expr(e2, var_symb)) {
                 (Num(n1), ast::BinOp::Add, Num(n2)) => {
                     Num(NumExpr::ArithOp(box n1, ArithOp::Add, box n2))
                 }
@@ -91,10 +92,10 @@ pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>) -> IRExpr 
                 ))),
                 (Bool(b1), ast::BinOp::And, Bool(b2)) => {
                     Bool(BoolExpr::BoolOp(box b1, BoolOp::And, box b2))
-                },
+                }
                 (Bool(b1), ast::BinOp::Or, Bool(b2)) => {
                     Bool(BoolExpr::BoolOp(box b1, BoolOp::Or, box b2))
-                },
+                }
                 (Bool(b1), ast::BinOp::Eq, Bool(b2)) => Bool(BoolExpr::Not(box BoolExpr::BoolOp(
                     box b1,
                     BoolOp::Xor,
@@ -108,12 +109,73 @@ pub fn translate_expr<'a>(WrapSpan(_, ast_expr): ExprSpan<'a, usize>) -> IRExpr 
                 }
                 _ => panic!("Shouldn't reach this!"),
             }
-        },
+        }
         _ => panic!("Shouldn't reach this!"),
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{frontend::ast::BinOp, intermediate};
+
     use super::*;
+
+    #[test]
+    fn check_boolean_expression() {
+        let var_symb = VariableSymbolTable::new();
+
+        let expr: WrapSpan<Expr<usize>> = WrapSpan(
+            "!true",
+            Expr::UnOp(UnOp::Neg, box WrapSpan("true", Expr::Bool(true))),
+        );
+
+        let translated: intermediate::Expr = Bool(BoolExpr::Not(box BoolExpr::Const(true)));
+
+        assert_eq!(translate_expr(expr, &var_symb), translated);
+
+        let expr2: WrapSpan<Expr<usize>> = WrapSpan(
+            "(ord 'a' == 65) || (true && !false)",
+            Expr::BinOp(
+                box WrapSpan(
+                    "ord 'a' == 65",
+                    Expr::BinOp(
+                        box WrapSpan(
+                            "ord 'a'",
+                            Expr::UnOp(UnOp::Ord, box WrapSpan("'a'", Expr::Char('a'))),
+                        ),
+                        BinOp::Eq,
+                        box WrapSpan("65", Expr::Int(65)),
+                    ),
+                ),
+                BinOp::Or,
+                box WrapSpan(
+                    "true && !false",
+                    Expr::BinOp(
+                        box WrapSpan("true", Expr::Bool(true)),
+                        BinOp::And,
+                        box WrapSpan(
+                            "!false",
+                            Expr::UnOp(UnOp::Neg, box WrapSpan("false", Expr::Bool(false))),
+                        ),
+                    ),
+                ),
+            ),
+        );
+
+        let translated2: intermediate::Expr = Bool(BoolExpr::BoolOp(
+            box BoolExpr::TestZero(NumExpr::ArithOp(
+                box NumExpr::Cast(DWord, box NumExpr::Const(Byte, 'a' as i32)),
+                ArithOp::Sub,
+                box NumExpr::Const(DWord, 65),
+            )),
+            BoolOp::Or,
+            box BoolExpr::BoolOp(
+                box BoolExpr::Const(true),
+                BoolOp::And,
+                box BoolExpr::Not(box BoolExpr::Const(false)),
+            ),
+        ));
+
+        assert_eq!(translate_expr(expr2, &var_symb), translated2);
+    }
 }
