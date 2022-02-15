4ecbf1d29689dfddede3b1b6015fa1cfc6ff028c
barci2 <bcieslar2001@gmail.com>
2022-02-15 15:41:30 +0000

    Linter fixes - function declaration formatting

--- src/backend/ssa_opt.rs
+++ src/backend/ssa_opt.rs
@@ -1,5 +1,8 @@
 use super::{ssa::StaticSingleAssignment, Options};
 
-pub(super) fn optimize_ssa(ssa: StaticSingleAssignment, options: &Options) -> StaticSingleAssignment {
+pub(super) fn optimize_ssa(
+    ssa: StaticSingleAssignment,
+    options: &Options,
+) -> StaticSingleAssignment {
     todo!()
 }
