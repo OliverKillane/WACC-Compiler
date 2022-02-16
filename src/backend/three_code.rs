2e66f11eabece3c7f25e7b41a253d2645d047ded
barci2 <bcieslar2001@gmail.com>
2022-02-16 21:19:15 +0000

    Comments for type definitions

--- src/backend/three_code.rs
+++ src/backend/three_code.rs
@@ -2,62 +2,109 @@ use super::Options;
 use crate::intermediate::{self as ir, DataRef, VarRepr};
 use std::collections::HashMap;
 
+/// Id of a statement in a statement graph
 type StatId = usize;
 
+/// Type of the source operand for an operation
 enum OpType {
-    DataRef(DataRef),
+    /// Constant value
     Const(u32),
+    /// Data reference to the static data in the static data vector in [program](ThreeCode)
+    DataRef(DataRef),
+    /// Variable with a given [id](VarRepr)
     Var(VarRepr),
 }
 
-enum OpSize {
+/// Size of the source operand
+enum Size {
+    /// 1 byte
     Byte,
+    /// 2 bytes
     Word,
+    /// 4 bytes
     DWord,
 }
 
-struct OpSrc(OpType, OpSize);
+/// Operand with its size and type
+struct OpSrc(OpType, Size);
 
+/// Binary operation code
 enum BinOp {
+    /// Addition (+)
     Add,
+    /// Subtraction (-)
     Sub,
+    /// Multiplication (*)
     Mul,
+    /// Division (/)
     Div,
+    /// Modulo (%)
     Mod,
 
+    /// Equality (==)
     Eq,
+    /// Not equality (!=)
     Ne,
+    /// Greater than (>)
     Gt,
+    /// Greater than or equal (>=)
     Gte,
+    /// Less than (<)
     Lt,
+    /// Less than or equal (<=)
     Lte,
 
+    /// Logical And (&&)
     And,
+    /// Logical Or (||)
     Or,
+    /// Logical Xor (^)
     Xor,
 }
 
-enum UnOp {
-    Not,
-    Pos,
-    Neg,
-    Zero,
-}
-
+/// Statement type
 enum StatCode {
-    AssignBinOp(VarRepr, OpSrc, BinOp, OpSrc),
-    AssignUnOp(VarRepr, UnOp, OpSrc),
-    Load(VarRepr, DataRef),
-    Store(DataRef, VarRepr),
-    Call(String, Vec<VarRepr>),
+    /// Assignment of a binary operation to a variable
+    Assign(VarRepr, OpSrc, BinOp, OpSrc),
+    /// Load from a reference to static data. The
+    /// number of bytes loaded is signified by the [size](Size) field.
+    LoadImm(VarRepr, DataRef, Size),
+    /// Load from a reference to a pointer. The first variable reference is
+    /// the load destination and the second one is the pointer to the data. The
+    /// number of bytes loaded is signified by the [size](Size) field.
+    LoadVar(VarRepr, VarRepr, Size),
+    /// Store to a static data reference. The number of bytes stored is signified
+    /// by the [size](Size) field.
+    StoreImm(DataRef, VarRepr, Size),
+    /// Store to a pointer reference. The first variable reference is the pointer to the
+    /// store destination and the second one is the variable to store the data from.
+    /// The number of bytes stored is signified by the [size](Size) field.
+    StoreVar(VarRepr, VarRepr, Size),
+    /// A call to a function. If the function name is not in the list of the
+    /// [program](ThreeCode) functions then it is assumed to be external and linked
+    /// to by the linker.
+    Call(VarRepr, String, Vec<VarRepr>),
 }
 
+/// Single statement in a dataflow graph - possible incoming statements,
+/// statement type, conditional jumps to other statements (evaluated consecutively)
+/// and the else branch if all other statements are equal to 0.
 struct Stat(Vec<StatId>, StatCode, Vec<(VarRepr, StatId)>, StatId);
 
+/// Graph of statements. The index of a statement signifies the
+/// [statement id](StatId) of that statement. The evaluation of the statement
+/// graph starts at the first statement.
 type StatGraph = Vec<Stat>;
 
+/// Function representation. The first vector are the variables to which the
+/// arguments will be assigned to and the [statement graph](StatGraph) is the dataflow graph
+/// that is evaluated.
 struct Function(Vec<VarRepr>, StatGraph);
 
+/// The entire program in the three-code representation. The first map is a map
+/// of all functions defined by the program. The [statement graph](StatGraph) is
+/// the main body of the program. The last map is a map of all statically-defined
+/// data in the program.
 pub(super) struct ThreeCode(
     HashMap<String, Function>,
     StatGraph,
