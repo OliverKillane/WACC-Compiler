afc9fe67fe93332cd6804ec66ce67adc13d61743
Cieslar, Bartlomiej <bartlomiej.cieslar20@imperial.ac.uk>
2022-02-16 02:29:57 +0000

    Backend skeleton

---
+++ src/backend/ssa.rs
@@ -0,0 +1,9 @@
+use super::three_code::ThreeCode;
+
+pub(super) struct StaticSingleAssignment;
+
+impl From<ThreeCode> for StaticSingleAssignment {
+    fn from(three_code: ThreeCode) -> StaticSingleAssignment {
+        todo!()
+    }
+}
