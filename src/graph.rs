d16ada10b42958b44ed502b91968c6d9c97b3f00
barci2 <bcieslar2001@gmail.com>
2022-02-21 16:12:44 +0000

    Graph structure

---
+++ src/graph.rs
@@ -0,0 +1,171 @@
+use std::{
+    cell::{Ref, RefCell, RefMut},
+    collections::HashSet,
+    fmt::Debug,
+    hash::Hash,
+    mem,
+    rc::Rc,
+};
+
+/// Node graph. Houses all the [nodes](NodeRef). [Deletes](Deleted) nodes only when they
+/// get deleted explicitly or when the graph goes out of scope (gets dropped).
+#[derive(Debug, Clone)]
+pub struct Graph<T: Deleted>(HashSet<NodeRef<T>>);
+
+/// A node reference. Represents a node in the graph.
+pub struct NodeRef<T: Deleted>(Rc<RefCell<T>>);
+
+/// A trait used in the graph to delete nodes whenever they become obsolete.
+pub trait Deleted {
+    fn deleted() -> Self;
+}
+
+impl<T: Deleted> Graph<T> {
+    pub fn new() -> Self {
+        Graph(HashSet::new())
+    }
+
+    pub fn new_node(&mut self, node: T) -> NodeRef<T> {
+        let node_ref = NodeRef(Rc::new(RefCell::new(node)));
+        self.0.insert(node_ref.clone());
+        node_ref
+    }
+
+    /// Description
+    pub fn remove_node(&mut self, node_ref: NodeRef<T>) {
+        self.0.remove(&node_ref);
+        node_ref.delete();
+    }
+
+    pub fn iter(&self) -> <HashSet<NodeRef<T>> as IntoIterator>::IntoIter {
+        self.0.clone().into_iter()
+    }
+}
+
+impl<T: Deleted> Drop for Graph<T> {
+    fn drop(&mut self) {
+        self.0.iter().for_each(NodeRef::delete);
+    }
+}
+
+impl<T: Deleted> IntoIterator for &Graph<T> {
+    type Item = NodeRef<T>;
+    type IntoIter = <HashSet<NodeRef<T>> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.iter()
+    }
+}
+
+impl<T: Deleted> NodeRef<T> {
+    pub fn set(&self, mut data: T) -> T {
+        mem::swap(&mut data, &mut self.0.borrow_mut());
+        data
+    }
+
+    pub fn get(&self) -> Ref<'_, T> {
+        self.0.borrow()
+    }
+
+    pub fn get_mut(&self) -> RefMut<'_, T> {
+        self.0.borrow_mut()
+    }
+
+    fn delete(&self) {
+        *self.0.borrow_mut() = T::deleted()
+    }
+}
+
+impl<T: Deleted + Debug> Debug for NodeRef<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "NodeRef{{{:?}}}", *self.0.borrow())
+    }
+}
+
+impl<T: Deleted> Clone for NodeRef<T> {
+    fn clone(&self) -> Self {
+        NodeRef(self.0.clone())
+    }
+}
+
+impl<T: Deleted> Hash for NodeRef<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_usize(self.0.as_ptr() as usize);
+    }
+}
+
+impl<T: Deleted> PartialEq for NodeRef<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.as_ptr() == other.0.as_ptr()
+    }
+}
+
+impl<T: Deleted> Eq for NodeRef<T> {}
+
+mod test {
+    use super::*;
+
+    struct Callback<'l>(Rc<RefCell<dyn FnMut()>>, Option<NodeRef<Callback<'l>>>);
+    impl<'l> Deleted for Callback<'l> {
+        fn deleted() -> Self {
+            Callback(Rc::new(RefCell::new(|| {})), None)
+        }
+    }
+    impl<'l> Drop for Callback<'l> {
+        fn drop(&mut self) {
+            let mut tmp_fn: Rc<RefCell<dyn FnMut() + '_>> = Rc::new(RefCell::new(|| {}));
+            mem::swap(&mut self.0, &mut tmp_fn);
+            tmp_fn.borrow_mut()();
+        }
+    }
+
+    #[test]
+    fn single_removed() {
+        let c = Rc::new(RefCell::new(0));
+        let c_closure = c.clone();
+        let c_add = Rc::new(RefCell::new(move || *c_closure.borrow_mut() += 1));
+        {
+            let mut g = Graph::new();
+            let _a = g.new_node(Callback(c_add.clone(), None));
+        }
+        assert_eq!(*c.borrow_mut(), 1);
+    }
+
+    #[test]
+    fn cycle_removed() {
+        let count = Rc::new(RefCell::new(0));
+        let count_closure = count.clone();
+        let count_add: Rc<RefCell<dyn FnMut() + '_>> =
+            Rc::new(RefCell::new(move || *count_closure.borrow_mut() += 1));
+        {
+            let mut g = Graph::new();
+            let a = g.new_node(Callback(count_add.clone(), None));
+            let b = g.new_node(Callback(count_add.clone(), None));
+            let c = g.new_node(Callback(count_add.clone(), None));
+            a.get_mut().1 = Some(b.clone());
+            b.get_mut().1 = Some(c.clone());
+            c.get_mut().1 = Some(a.clone());
+        }
+        assert_eq!(*count.borrow_mut(), 3);
+    }
+
+    #[test]
+    fn node_removed() {
+        let count = Rc::new(RefCell::new(0));
+        let count_closure = count.clone();
+        let count_add: Rc<RefCell<dyn FnMut() + '_>> =
+            Rc::new(RefCell::new(move || *count_closure.borrow_mut() += 1));
+        {
+            let mut g = Graph::new();
+            let a = g.new_node(Callback(count_add.clone(), None));
+            let b = g.new_node(Callback(count_add.clone(), None));
+            let c = g.new_node(Callback(count_add.clone(), None));
+            a.get_mut().1 = Some(b.clone());
+            b.get_mut().1 = Some(c.clone());
+            c.get_mut().1 = Some(a.clone());
+            g.remove_node(a);
+            assert_eq!(*count.borrow_mut(), 1);
+        }
+        assert_eq!(*count.borrow_mut(), 3);
+    }
+}
