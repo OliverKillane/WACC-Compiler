b72b25f04672be83f692dca7c5c7df8b39b9fb35
barci2 <bcieslar2001@gmail.com>
2022-02-22 15:03:01 +0000

    Changes necessary for the translation to work

--- src/backend/three_code.rs
+++ src/backend/three_code.rs
@@ -1,22 +1,23 @@
 use super::Options;
+use crate::graph::{Deleted, Graph, NodeRef};
 use crate::intermediate::{self as ir, DataRef, VarRepr};
 use std::collections::HashMap;
+use std::mem;
 
-/// Id of a statement in a statement graph
-type StatId = usize;
-
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 /// Type of the source operand for an operation
-enum OpSrc {
+pub(super) enum OpSrc {
     /// Constant value
-    Const(u32),
+    Const(i32),
     /// Value of a data reference to the static data in the static data vector in [program](ThreeCode)
-    DataRef(DataRef),
+    DataRef(DataRef, i32),
     /// Variable with a given [id](VarRepr)
     Var(VarRepr),
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 /// Size of the load/store operations
-enum Size {
+pub(super) enum Size {
     /// 1 byte
     Byte,
     /// 2 bytes
@@ -25,8 +26,9 @@ enum Size {
     DWord,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 /// Binary operation code
-enum BinOp {
+pub(super) enum BinOp {
     /// Addition (+)
     Add,
     /// Subtraction (-)
@@ -59,60 +61,140 @@ enum BinOp {
     Xor,
 }
 
+#[derive(Debug, PartialEq, Eq, Clone)]
 /// Statement type
-enum StatCode {
+pub(super) enum StatCode {
     /// Assignment of one variable to another
     Assign(VarRepr, OpSrc),
     /// Assignment of a binary operation to a variable
     AssignOp(VarRepr, OpSrc, BinOp, OpSrc),
-    /// Load from a reference to static data. The
-    /// number of bytes loaded is signified by the [size](Size) field.
-    LoadImm(VarRepr, DataRef, Size),
     /// Load from a reference to a pointer. The first variable reference is
     /// the load destination and the second one is the pointer to the data. The
     /// number of bytes loaded is signified by the [size](Size) field.
-    LoadVar(VarRepr, VarRepr, Size),
-    /// Store to a static data reference. The number of bytes stored is signified
-    /// by the [size](Size) field.
-    StoreImm(DataRef, VarRepr, Size),
+    Load(VarRepr, VarRepr, Size),
     /// Store to a pointer reference. The first variable reference is the pointer to the
     /// store destination and the second one is the variable to store the data from.
     /// The number of bytes stored is signified by the [size](Size) field.
-    StoreVar(VarRepr, VarRepr, Size),
+    Store(VarRepr, VarRepr, Size),
     /// A call to a function. If the function name is not in the list of the
     /// [program](ThreeCode) functions then it is assumed to be external and linked
     /// to by the linker.
     Call(VarRepr, String, Vec<VarRepr>),
+    /// A call to a function that ommits the return value. If the function name
+    /// is not in the list of the [program](ThreeCode) functions then it is assumed
+    /// to be external and linked to by the linker.
+    VoidCall(String, Vec<VarRepr>),
+    /// Returns from the function.
+    Return(VarRepr),
 }
 
-/// Single statement in a dataflow graph - possible incoming statements,
-/// statement type, conditional jumps to other statements (evaluated consecutively)
-/// and the else branch if all other statements are equal to 0.
-struct Stat(Vec<StatId>, StatCode, Vec<(VarRepr, StatId)>, StatId);
+/// General type of the statement. Used in the dataflow graph.
+#[derive(Debug, Clone)]
 
-/// Graph of statements. The index of a statement signifies the
-/// [statement id](StatId) of that statement. The evaluation of the statement
-/// graph starts at the first statement.
-type StatGraph = Vec<Stat>;
+pub(super) enum StatType {
+    /// Used internally for the purposes of creating the graph.
+    Dummy(Vec<StatNode>),
+    /// Simple data manipulaiton statement that has a superceding statement.
+    Simple(Vec<StatNode>, StatCode, StatNode),
+    /// Data manipulation statement that is expected to finish the execution one
+    /// way or another.
+    Final(Vec<StatNode>, StatCode),
+    /// A simple branch instruction that checks if the value in the variable is
+    /// a boolean 1 or a 0.
+    Branch(Vec<StatNode>, VarRepr, StatNode, StatNode),
+    /// A self-looping infinite loop, which might occur as a user program.
+    Loop(Vec<StatNode>),
+}
 
-/// Local variables that have to be represented in memory during program execution.
-type LocalVars = HashMap<VarRepr, DataRef>;
+/// A [statement graph](StatGraph) node.
+type StatNode = NodeRef<StatType>;
 
+#[derive(Debug, Clone)]
 /// Function representation. The first vector are the variables to which the
 /// arguments will be assigned to and the [statement graph](StatGraph) is the dataflow graph
 /// that is evaluated.
-struct Function(Vec<VarRepr>, LocalVars, StatGraph);
+pub(super) struct Function {
+    pub args: Vec<VarRepr>,
+    pub code: Option<StatNode>,
+    pub read_ref: Option<DataRef>,
+}
 
+#[derive(Debug, Clone)]
 /// The entire program in the three-code representation. The first map is a map
 /// of all functions defined by the program. The [statement graph](StatGraph) is
 /// the main body of the program. The last map is a map of all statically-defined
 /// data in the program.
-pub(super) struct ThreeCode(
-    HashMap<String, Function>,
-    LocalVars,
-    StatGraph,
-    HashMap<DataRef, Vec<u8>>,
-);
+pub(super) struct ThreeCode {
+    pub functions: HashMap<String, Function>,
+    pub data_refs: HashMap<DataRef, Vec<u8>>,
+    pub graph: Graph<StatType>,
+    pub read_ref: Option<DataRef>,
+    pub code: Option<StatNode>,
+}
+
+impl StatType {
+    fn new_final(stat_code: StatCode) -> Self {
+        StatType::Final(Vec::new(), stat_code)
+    }
+
+    fn new_simple(stat_code: StatCode, next: StatNode) -> Self {
+        StatType::Simple(Vec::new(), stat_code, next)
+    }
+
+    fn new_branch(cond: VarRepr, if_true: StatNode, if_false: StatNode) -> Self {
+        StatType::Branch(Vec::new(), cond, if_true, if_false)
+    }
+
+    fn new_loop() -> Self {
+        StatType::Loop(Vec::new())
+    }
+
+    fn add_incoming(&mut self, node: StatNode) {
+        match self {
+            Self::Simple(incoming, _, _)
+            | Self::Final(incoming, _)
+            | Self::Branch(incoming, _, _, _)
+            | Self::Loop(incoming)
+            | Self::Dummy(incoming) => incoming.push(node),
+        }
+    }
+
+    fn set_incoming(&mut self, incoming: Vec<StatNode>) {
+        match self {
+            Self::Simple(old_incoming, _, _)
+            | Self::Final(old_incoming, _)
+            | Self::Branch(old_incoming, _, _, _)
+            | Self::Loop(old_incoming)
+            | Self::Dummy(old_incoming) => *old_incoming = incoming,
+        }
+    }
+
+    fn incoming(&self) -> Vec<StatNode> {
+        match self {
+            Self::Simple(incoming, _, _)
+            | Self::Final(incoming, _)
+            | Self::Branch(incoming, _, _, _)
+            | Self::Loop(incoming)
+            | Self::Dummy(incoming) => incoming.clone(),
+        }
+    }
+
+    fn append(&mut self, node: StatNode) {
+        let mut tmp_node = Self::deleted();
+        mem::swap(self, &mut tmp_node);
+        tmp_node = match tmp_node {
+            Self::Final(incoming, stat_code) => Self::Simple(incoming, stat_code, node),
+            _ => panic!("Node not final"),
+        };
+        mem::swap(self, &mut tmp_node);
+    }
+}
+
+impl Deleted for StatType {
+    fn deleted() -> Self {
+        Self::Dummy(Vec::new())
+    }
+}
 
 impl From<(ir::Program, &Options)> for ThreeCode {
     fn from((program, options): (ir::Program, &Options)) -> ThreeCode {
