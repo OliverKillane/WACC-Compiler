32df936115e61319b5deccb4059c2f2b050493dd
Oliver Killane <oliver.killane20@imperial.ac.uk>
2022-02-26 21:39:20 +0000

    Changed arm representation to be graph based [ ok220 ]

--- src/backend/arm/arm_repr.rs
+++ src/backend/arm/arm_repr.rs
@@ -1,6 +1,34 @@
 //! A structure representing a subset of arm assembly, to be generated as the
 //! final stage of the compiler, and written to a file (formatted)
-use super::int_constraints::ConstrainedInt;
+use super::{
+    super::super::graph::{Deleted, Graph, NodeRef},
+    int_constraints::ConstrainedInt,
+};
+use std::collections::HashMap;
+
+/// The temporary type (used before register allocation)
+pub type Temporary = usize;
+
+/// All general purpose register accessible in user mode (registers are
+/// allocated in a second pass).
+pub enum Register {
+    R0,
+    R1,
+    R2,
+    R3,
+    R4,
+    R5,
+    R6,
+    R7,
+    R8,
+    R9,
+    R10,
+    R11,
+    R12,
+    Sp,
+    Lr,
+    Pc,
+}
 
 /// Condition suffixes to be used in conditionally executing instructions.
 pub enum Cond {
@@ -21,26 +49,7 @@ pub enum Cond {
     Al,
 }
 
-/// All general purpose register accessible in user mode.
-pub enum Register {
-    R0,
-    R1,
-    R2,
-    R3,
-    R4,
-    R5,
-    R6,
-    R7,
-    R8,
-    R9,
-    R10,
-    R11,
-    R12,
-    Sp,
-    Lr,
-    Pc,
-}
-
+/// The shifts that can be used for operands.
 #[derive(Clone, Copy)]
 pub enum Shift {
     /// Arithmetic Right Shift, copies the sign bit for two complement to fill
@@ -56,14 +65,15 @@ pub enum Shift {
     Rxx,
 }
 
-pub enum FlexOperand {
+/// The allowed flexible second operands for arm.
+pub enum FlexOperand<IdentType> {
     /// Use an immediate operand, must be a shifted 8-bit pattern
     Imm(u32),
     /// Shift register to get operand value
     ///
     /// Note:
     /// - Register cannot be `R15`/`PC`
-    ShiftReg(Register, Option<Shift>),
+    ShiftReg(IdentType, Option<Shift>),
 }
 
 /// Regular operations, two operands and a destination.
@@ -157,20 +167,20 @@ pub enum MemOp {
 }
 
 /// Memory operand is the second operand of the [memory operand](MemOp).
-pub enum MemOperand {
-    /// Indirect memory location stored in [Register].
-    Zero(Register),
-    /// Indirect memory location stored in [Register] with offset [FlexOffset].
+pub enum MemOperand<IdentType> {
+    /// Indirect memory location stored in an identifier.
+    Zero(IdentType),
+    /// Indirect memory location stored in an identifier with offset [FlexOffset].
     /// If [bool] is true then we update the value in the register with offset
     /// before accessing the memory location.
-    PreIndex(Register, FlexOffset, bool),
+    PreIndex(IdentType, FlexOffset, bool),
     /// Direct from the label in the data section indicated by [String].
     Label(String),
     /// Immediate 32-bit integer value.
     Expression(i32),
-    /// Indirect memory location stored in [Register]. [Register] is updated by
+    /// Indirect memory location stored in the identifier, updated by
     /// [FlexOffset] after the memory access has taken place.
-    PostIndex(Register, FlexOffset),
+    PostIndex(IdentType, FlexOffset),
 }
 
 /// Offset for LDR/STR instructions.
@@ -182,7 +192,7 @@ pub enum FlexOffset {
     ShiftReg(bool, Register, Option<Shift>),
 }
 
-pub enum Stat {
+pub enum Stat<IdentType> {
     /// A normal register operation of form:
     /// ```text
     /// (Operation, Condition, Set Condition Bits?, Register Destination, Register Operand, Flexible Second Operand)
@@ -191,72 +201,110 @@ pub enum Stat {
     /// Note:
     /// - `R15`/`PC` should not be used with the S suffix (set condition bits), as
     ///  this is undefined behaviour, and the assembler will not warn.
-    ApplyOp(RegOp, Cond, bool, Register, Register, FlexOperand),
+    ApplyOp(
+        RegOp,
+        Cond,
+        bool,
+        IdentType,
+        IdentType,
+        FlexOperand<IdentType>,
+    ),
 
     /// Basic multiplication taking only the 32 least significant bits
-    Mul(Cond, bool, Register, Register, Register),
+    Mul(Cond, bool, IdentType, IdentType, IdentType),
 
     /// Basic Multiplication with addition added. Takes 32 least significant
     /// bits.
-    MulA(Cond, bool, Register, Register, Register, Register),
+    MulA(Cond, bool, IdentType, IdentType, IdentType, IdentType),
 
     /// A multiply operation using 4 register.
     /// ```text
     /// (Multiply Operation, Condition, Set Condition Bits?, Result Hight, Result Low, Register Operand, Second Register Operand)
     /// ```
-    MulOp(MulOp, Cond, bool, Register, Register, Register, Register),
+    MulOp(
+        MulOp,
+        Cond,
+        bool,
+        IdentType,
+        IdentType,
+        IdentType,
+        IdentType,
+    ),
 
     /// A move instruction from register to register of form:
     /// ```text
     /// (Move Operation, Condition, Set Condition Bits?, Register Destination, Flexible Second Operand)
     /// ```
-    Move(MovOp, Cond, bool, Register, FlexOperand),
+    Move(MovOp, Cond, bool, IdentType, FlexOperand<IdentType>),
 
     /// A comparison instruction to set the condition bits.
     /// ```text
     /// (Comparison Operation, Condition, Register Operand, Flexible Second Operand)
     /// ```
-    Cmp(CmpOp, Cond, Register, FlexOperand),
+    Cmp(CmpOp, Cond, Register, IdentType),
 
     /// A saturating operation, checks for overflows/underflows.
     /// ```text
     /// (Saturating operation, Register Desination, Register Operand, Second Register Operand)
     /// ```
-    SatOp(SatOp, Cond, Register, Register, Register),
+    SatOp(SatOp, Cond, IdentType, IdentType, IdentType),
 
     /// Read the CSPR register into a general purpose register.
     /// ```text
     /// Flag:  N  |Z  |C  |V  |Q
     ///  Bit: 31 |30 |29 |28 |27
     /// ```
-    ReadCPSR(Register),
+    ReadCPSR(IdentType),
+
+    /// Conditional branch to a label.
+    /// ```text
+    /// (Type of branch: B/BL, Condition, Label to branch to)
+    /// ```
+
+    /// Memory Operation (a load or store).
+    MemOp(MemOp, Cond, bool, IdentType, MemOperand<IdentType>),
+
+    /// Push thumb instruction.
+    Push(Cond, Vec<IdentType>),
+
+    /// Pop thumb instruction, R15/PC can only be used as the last register to
+    /// pop (prevent jump before popping other registers).
+    Pop(Cond, Vec<IdentType>),
+}
+
+pub type ArmNode<IdentType> = NodeRef<ControlFlow<IdentType>>;
+pub enum ControlFlow<IdentType> {
+    /// The start of a control flow path (reference is to next node)
+    Start(ArmNode<IdentType>),
+
+    /// Simple Control Flow
+    Simple(ArmNode<IdentType>, Stat<IdentType>, ArmNode<IdentType>),
 
     /// Branch to a label.
     /// ```text
     /// (Type of branch: B/BL, Condition, Label to branch to)
     /// ```
-    Branch(BranchOp, Cond, String),
+    Branching(
+        ArmNode<IdentType>,
+        ArmNode<IdentType>,
+        Option<(Cond, ArmNode<IdentType>)>,
+    ),
 
-    /// Memory Operation (a load or store).
-    MemOp(MemOp, Cond, bool, Register, MemOperand),
+    /// Call to a function (branch and link)
+    Call(
+        ArmNode<IdentType>,
+        String,
+        Option<(Cond, ArmNode<IdentType>)>,
+    ),
 
-    /// Push thumb instruction.
-    Push(Cond, Vec<Register>),
+    /// A position that can be jumped to from multiple locations/can have multiple predecessors.
+    Multi(Vec<ArmNode<IdentType>>, ArmNode<IdentType>),
 
-    /// Pop thumb instruction, R15/PC can only be used as the last register to
-    /// pop (prevent jump before popping other registers).
-    Pop(Cond, Vec<Register>),
+    /// The end of a control flow path
+    End(ArmNode<IdentType>),
 
-    /// A literal pool assembler directive, allowing the compiler to place parts
-    /// of the data section in the pool so they are in range of the instructions
-    /// that use them.
-    LiteralPool,
-
-    /// Declares a label global.
-    Global(String),
-
-    /// Label to allow branches, labels must be unique.
-    Label(String),
+    /// Used to remove references when dropping elements of the graph
+    Removed,
 }
 
 pub enum DataKind {
@@ -267,5 +315,21 @@ pub enum DataKind {
 /// All types of data that can be kept in the binary.
 pub struct Data(pub String, pub DataKind);
 
+pub struct Function<IdentType> {
+    pub args: Vec<IdentType>,
+    pub start_node: ArmNode<IdentType>,
+}
+
 /// The main program containing text ([instructions](Stat)) and [data](Data).
-pub struct Program(pub Vec<Data>, pub Vec<Stat>);
+pub struct Program<IdentType> {
+    pub data: Vec<Data>,
+    pub main: ArmNode<IdentType>,
+    pub functions: HashMap<String, Function<IdentType>>,
+    pub cfg: Graph<ControlFlow<IdentType>>,
+}
+
+impl<T> Deleted for ControlFlow<T> {
+    fn deleted() -> Self {
+        Self::Removed
+    }
+}
