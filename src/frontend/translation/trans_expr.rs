ffa6349d101f007a9a608d6ce13a84f74d69acbd
Panayiotis Gavriil <panayiotis.gavriil19@imperial.ac.uk>
2022-02-27 14:27:20 +0000

    Changed variable naming style

--- src/frontend/translation/trans_expr.rs
+++ src/frontend/translation/trans_expr.rs
@@ -20,33 +20,33 @@ pub fn translate_expr<'a>(
         // pair literal 'null' - maybe define null as PtrExpr in IR?
         Expr::Null => Ptr(PtrExpr::Null),
 
-        Expr::Int(i) => Num(NumExpr::Const(DWord, i)),
+        Expr::Int(int) => Num(NumExpr::Const(DWord, int)),
 
-        Expr::Bool(b) => Bool(BoolExpr::Const(b)),
+        Expr::Bool(bln) => Bool(BoolExpr::Const(bln)),
 
-        Expr::Char(c) => Num(NumExpr::Const(Byte, c as i32)),
+        Expr::Char(chr) => Num(NumExpr::Const(Byte, chr as i32)),
 
-        Expr::String(s) => {
+        Expr::String(str) => {
             let dataref: u64 = dataref_map.len() as u64;
             let mut char_vec: Vec<IRExpr> = Vec::new();
 
-            for c in s.chars() {
-                char_vec.push(Num(NumExpr::Const(Byte, c as i32)));
+            for chr in str.chars() {
+                char_vec.push(Num(NumExpr::Const(Byte, chr as i32)));
             }
 
             dataref_map.insert(dataref, char_vec);
             Ptr(PtrExpr::DataRef(dataref))
         }
 
-        Expr::Var(v) => match var_symb.get_type_from_id(v) {
-            Some(t) => match t {
-                ast::Type::Int => Num(NumExpr::Var(v)),
-                ast::Type::Bool => Bool(BoolExpr::Var(v)),
-                ast::Type::Char => Num(NumExpr::Var(v)),
-                ast::Type::String => Ptr(PtrExpr::Var(v)),
-                ast::Type::Any => Ptr(PtrExpr::Var(v)),
-                ast::Type::Pair(_, _) => Ptr(PtrExpr::Var(v)),
-                ast::Type::Array(_, _) => Ptr(PtrExpr::Var(v)),
+        Expr::Var(var) => match var_symb.get_type_from_id(var) {
+            Some(typ) => match typ {
+                ast::Type::Int => Num(NumExpr::Var(var)),
+                ast::Type::Bool => Bool(BoolExpr::Var(var)),
+                ast::Type::Char => Num(NumExpr::Var(var)),
+                ast::Type::String => Ptr(PtrExpr::Var(var)),
+                ast::Type::Any => Ptr(PtrExpr::Var(var)),
+                ast::Type::Pair(_, _) => Ptr(PtrExpr::Var(var)),
+                ast::Type::Array(_, _) => Ptr(PtrExpr::Var(var)),
                 _ => panic!("What are you even doing with your life by this point Oli? Semantic analyzer broken!")
             },
             None => panic!("What are you even doing with your life by this point Oli? Semantic analyzer broken!")
@@ -59,77 +59,77 @@ pub fn translate_expr<'a>(
             todo!()
         }
 
-        Expr::UnOp(o, box e) => match (o, translate_expr(e, var_symb, dataref_map)) {
-            (UnOp::Neg, Bool(b)) => Bool(BoolExpr::Not(box b)),
-            (UnOp::Minus, Num(n)) => Num(NumExpr::ArithOp(
+        Expr::UnOp(un_op, box expr_span) => match (un_op, translate_expr(expr_span, var_symb, dataref_map)) {
+            (UnOp::Neg, Bool(bln)) => Bool(BoolExpr::Not(box bln)),
+            (UnOp::Minus, Num(num)) => Num(NumExpr::ArithOp(
                 box NumExpr::Const(DWord, -1),
                 ArithOp::Mul,
-                box n,
+                box num,
             )),
-            (UnOp::Chr, Num(n)) => Num(NumExpr::Cast(Byte, box n)),
-            (UnOp::Ord, Num(n)) => Num(NumExpr::Cast(DWord, box n)),
-            (UnOp::Len, Ptr(p)) => Num(NumExpr::Deref(NumSize::DWord, p)),
-            (_, Ptr(p)) => todo!(),
+            (UnOp::Chr, Num(num)) => Num(NumExpr::Cast(Byte, box num)),
+            (UnOp::Ord, Num(num)) => Num(NumExpr::Cast(DWord, box num)),
+            (UnOp::Len, Ptr(ptr)) => Num(NumExpr::Deref(NumSize::DWord, ptr)),
+            (_, Ptr(ptr)) => todo!(),
             _ => panic!("What are you even doing with your life by this point Oli? Semantic analyzer broken!")
         },
 
         // Should change this so that boolean operations are short-circuited
-        Expr::BinOp(box e1, o, box e2) => {
+        Expr::BinOp(box expr_span1, bin_op, box expr_span2) => {
             match (
-                translate_expr(e1, var_symb, dataref_map),
-                o,
-                translate_expr(e2, var_symb, dataref_map),
+                translate_expr(expr_span1, var_symb, dataref_map),
+                bin_op,
+                translate_expr(expr_span2, var_symb, dataref_map),
             ) {
-                (Num(n1), ast::BinOp::Add, Num(n2)) => {
-                    Num(NumExpr::ArithOp(box n1, ArithOp::Add, box n2))
+                (Num(num_expr1), ast::BinOp::Add, Num(num_expr2)) => {
+                    Num(NumExpr::ArithOp(box num_expr1, ArithOp::Add, box num_expr2))
                 }
-                (Num(n1), ast::BinOp::Sub, Num(n2)) => {
-                    Num(NumExpr::ArithOp(box n1, ArithOp::Sub, box n2))
+                (Num(num_expr1), ast::BinOp::Sub, Num(num_expr2)) => {
+                    Num(NumExpr::ArithOp(box num_expr1, ArithOp::Sub, box num_expr2))
                 }
-                (Num(n1), ast::BinOp::Mul, Num(n2)) => {
-                    Num(NumExpr::ArithOp(box n1, ArithOp::Mul, box n2))
+                (Num(num_expr1), ast::BinOp::Mul, Num(num_expr2)) => {
+                    Num(NumExpr::ArithOp(box num_expr1, ArithOp::Mul, box num_expr2))
                 }
-                (Num(n1), ast::BinOp::Div, Num(n2)) => {
-                    Num(NumExpr::ArithOp(box n1, ArithOp::Div, box n2))
+                (Num(num_expr1), ast::BinOp::Div, Num(num_expr2)) => {
+                    Num(NumExpr::ArithOp(box num_expr1, ArithOp::Div, box num_expr2))
                 }
-                (Num(n1), ast::BinOp::Mod, Num(n2)) => {
-                    Num(NumExpr::ArithOp(box n1, ArithOp::Mod, box n2))
+                (Num(num_expr1), ast::BinOp::Mod, Num(num_expr2)) => {
+                    Num(NumExpr::ArithOp(box num_expr1, ArithOp::Mod, box num_expr2))
                 }
-                (Num(n1), ast::BinOp::Gt, Num(n2)) => Bool(BoolExpr::TestPositive(
-                    NumExpr::ArithOp(box n1, ArithOp::Sub, box n2),
+                (Num(num_expr1), ast::BinOp::Gt, Num(num_expr2)) => Bool(BoolExpr::TestPositive(
+                    NumExpr::ArithOp(box num_expr1, ArithOp::Sub, box num_expr2),
                 )),
-                (Num(n1), ast::BinOp::Gte, Num(n2)) => Bool(BoolExpr::Not(
-                    box BoolExpr::TestPositive(NumExpr::ArithOp(box n2, ArithOp::Sub, box n1)),
+                (Num(num_expr1), ast::BinOp::Gte, Num(num_expr2)) => Bool(BoolExpr::Not(
+                    box BoolExpr::TestPositive(NumExpr::ArithOp(box num_expr2, ArithOp::Sub, box num_expr1)),
                 )),
-                (Num(n1), ast::BinOp::Lt, Num(n2)) => Bool(BoolExpr::TestPositive(
-                    NumExpr::ArithOp(box n2, ArithOp::Sub, box n1),
+                (Num(num_expr1), ast::BinOp::Lt, Num(num_expr2)) => Bool(BoolExpr::TestPositive(
+                    NumExpr::ArithOp(box num_expr2, ArithOp::Sub, box num_expr1),
                 )),
-                (Num(n1), ast::BinOp::Lte, Num(n2)) => Bool(BoolExpr::Not(
-                    box BoolExpr::TestPositive(NumExpr::ArithOp(box n1, ArithOp::Sub, box n2)),
+                (Num(num_expr1), ast::BinOp::Lte, Num(num_expr2)) => Bool(BoolExpr::Not(
+                    box BoolExpr::TestPositive(NumExpr::ArithOp(box num_expr1, ArithOp::Sub, box num_expr2)),
                 )),
-                (Num(n1), ast::BinOp::Eq, Num(n2)) => Bool(BoolExpr::TestZero(NumExpr::ArithOp(
-                    box n1,
+                (Num(num_expr1), ast::BinOp::Eq, Num(num_expr2)) => Bool(BoolExpr::TestZero(NumExpr::ArithOp(
+                    box num_expr1,
                     ArithOp::Sub,
-                    box n2,
+                    box num_expr2,
                 ))),
-                (Num(n1), ast::BinOp::Ne, Num(n2)) => Bool(BoolExpr::Not(box BoolExpr::TestZero(
-                    NumExpr::ArithOp(box n1, ArithOp::Sub, box n2),
+                (Num(num_expr1), ast::BinOp::Ne, Num(num_expr2)) => Bool(BoolExpr::Not(box BoolExpr::TestZero(
+                    NumExpr::ArithOp(box num_expr1, ArithOp::Sub, box num_expr2),
                 ))),
-                (Bool(b1), ast::BinOp::And, Bool(b2)) => {
-                    Bool(BoolExpr::BoolOp(box b1, BoolOp::And, box b2))
+                (Bool(bln_expr1), ast::BinOp::And, Bool(bln_expr2)) => {
+                    Bool(BoolExpr::BoolOp(box bln_expr1, BoolOp::And, box bln_expr2))
                 }
-                (Bool(b1), ast::BinOp::Or, Bool(b2)) => {
-                    Bool(BoolExpr::BoolOp(box b1, BoolOp::Or, box b2))
+                (Bool(bln_expr1), ast::BinOp::Or, Bool(bln_expr2)) => {
+                    Bool(BoolExpr::BoolOp(box bln_expr1, BoolOp::Or, box bln_expr2))
                 }
-                (Bool(b1), ast::BinOp::Eq, Bool(b2)) => Bool(BoolExpr::Not(box BoolExpr::BoolOp(
-                    box b1,
+                (Bool(bln_expr1), ast::BinOp::Eq, Bool(bln_expr2)) => Bool(BoolExpr::Not(box BoolExpr::BoolOp(
+                    box bln_expr1,
                     BoolOp::Xor,
-                    box b2,
+                    box bln_expr2,
                 ))),
-                (Bool(b1), ast::BinOp::Ne, Bool(b2)) => {
-                    Bool(BoolExpr::BoolOp(box b1, BoolOp::Xor, box b2))
+                (Bool(bln_expr1), ast::BinOp::Ne, Bool(bln_expr2)) => {
+                    Bool(BoolExpr::BoolOp(box bln_expr1, BoolOp::Xor, box bln_expr2))
                 }
-                (e1, ast::BinOp::Newpair, e2) => {
+                (expr1, ast::BinOp::Newpair, expr2) => {
                     todo!()
                 }
                 _ => panic!("What are you even doing with your life by this point Oli? Semantic analyzer broken!"),
