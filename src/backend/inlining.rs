6abc621a123c49bf62816740036ee2ae65aef936
barci2 <bcieslar2001@gmail.com>
2022-03-11 08:41:23 +0000

    Main inlining heuristic algorithm done

--- src/backend/inlining.rs
+++ src/backend/inlining.rs
@@ -1,5 +1,7 @@
 use std::{
-    collections::{HashMap, HashSet},
+    collections::{HashMap, HashSet, LinkedList},
+    intrinsics::ceilf64,
+    iter::zip,
     rc::Rc,
 };
 
@@ -120,17 +122,24 @@ fn get_function_sccs<'l>(
     sccs
 }
 
-fn get_mapping(var: VarRepr, variable_mappings: &mut HashMap<VarRepr, VarRepr>) -> VarRepr {
-    let variable_mappings_size = variable_mappings.len();
-    *variable_mappings
-        .entry(var)
-        .or_insert(variable_mappings_size)
+fn get_mapping(
+    var: VarRepr,
+    new_variable: &mut VarRepr,
+    variable_mappings: &mut HashMap<VarRepr, VarRepr>,
+) -> VarRepr {
+    let mapping = *variable_mappings.entry(var).or_insert(*new_variable);
+    *new_variable += 1;
+    mapping
 }
 
-fn substitute_op_src(op_src: OpSrc, variable_mappings: &mut HashMap<VarRepr, VarRepr>) -> OpSrc {
+fn substitute_op_src(
+    op_src: OpSrc,
+    new_variable: &mut VarRepr,
+    variable_mappings: &mut HashMap<VarRepr, VarRepr>,
+) -> OpSrc {
     if let OpSrc::Var(var) = op_src {
         let variable_mappings_size = variable_mappings.len();
-        OpSrc::Var(get_mapping(var, variable_mappings))
+        OpSrc::Var(get_mapping(var, new_variable, variable_mappings))
     } else {
         op_src
     }
@@ -138,51 +147,64 @@ fn substitute_op_src(op_src: OpSrc, variable_mappings: &mut HashMap<VarRepr, Var
 
 fn substitute_vars(
     stat_code: &StatCode,
+    new_variable: &mut VarRepr,
     variable_mappings: &mut HashMap<VarRepr, VarRepr>,
 ) -> StatCode {
     match stat_code {
         StatCode::Assign(var, op_src) => StatCode::Assign(
-            get_mapping(*var, variable_mappings),
-            substitute_op_src(*op_src, variable_mappings),
+            get_mapping(*var, new_variable, variable_mappings),
+            substitute_op_src(*op_src, new_variable, variable_mappings),
         ),
         StatCode::AssignOp(var, op_src1, bin_op, op_src2) => StatCode::AssignOp(
-            get_mapping(*var, variable_mappings),
-            substitute_op_src(*op_src1, variable_mappings),
+            get_mapping(*var, new_variable, variable_mappings),
+            substitute_op_src(*op_src1, new_variable, variable_mappings),
             *bin_op,
-            substitute_op_src(*op_src2, variable_mappings),
+            substitute_op_src(*op_src2, new_variable, variable_mappings),
         ),
         StatCode::Load(var1, var2, size) => StatCode::Load(
-            get_mapping(*var1, variable_mappings),
-            get_mapping(*var2, variable_mappings),
+            get_mapping(*var1, new_variable, variable_mappings),
+            get_mapping(*var2, new_variable, variable_mappings),
             *size,
         ),
         StatCode::Store(var1, var2, size) => StatCode::Store(
-            get_mapping(*var1, variable_mappings),
-            get_mapping(*var2, variable_mappings),
+            get_mapping(*var1, new_variable, variable_mappings),
+            get_mapping(*var2, new_variable, variable_mappings),
             *size,
         ),
         StatCode::Call(var, fname, args) => StatCode::Call(
-            get_mapping(*var, variable_mappings),
+            get_mapping(*var, new_variable, variable_mappings),
             fname.clone(),
             args.iter()
-                .map(|var| get_mapping(*var, variable_mappings))
+                .map(|var| get_mapping(*var, new_variable, variable_mappings))
                 .collect(),
         ),
         StatCode::VoidCall(fname, args) => StatCode::VoidCall(
             fname.clone(),
             args.iter()
-                .map(|var| get_mapping(*var, variable_mappings))
+                .map(|var| get_mapping(*var, new_variable, variable_mappings))
                 .collect(),
         ),
     }
 }
 
+fn get_call_name(node: &StatNode) -> Option<&str> {
+    if let StatType::Simple(_, StatCode::VoidCall(fname, _) | StatCode::Call(_, fname, _), _) =
+        &*node.get()
+    {
+        Some(fname)
+    } else {
+        None
+    }
+}
+
 fn inline_code_dfs(
+    new_call_nodes: &mut LinkedList<StatNode>,
     node: &StatNode,
     mappings: &mut HashMap<StatNode, StatNode>,
     new_graph: &mut Graph<StatType>,
     call_successor: StatNode,
     assigned_var: Option<VarRepr>,
+    new_variable: &mut VarRepr,
     variable_mappings: &mut HashMap<VarRepr, VarRepr>,
 ) -> StatNode {
     if mappings.contains_key(node) {
@@ -192,19 +214,24 @@ fn inline_code_dfs(
         StatType::Simple(_, stat_code, next_node) => {
             let new_node = new_graph.new_node(StatType::deleted());
             mappings.insert(node.clone(), new_node.clone());
+            if get_call_name(node).is_some() {
+                new_call_nodes.push_back(new_node);
+            }
             let next_node = inline_code_dfs(
+                new_call_nodes,
                 next_node,
                 mappings,
                 new_graph,
                 call_successor,
                 assigned_var,
+                new_variable,
                 variable_mappings,
             );
             next_node.get().add_incoming(new_node.clone());
             let incoming = new_node.get().incoming();
             new_node.set(StatType::Simple(
                 incoming,
-                substitute_vars(stat_code, variable_mappings),
+                substitute_vars(stat_code, new_variable, variable_mappings),
                 next_node,
             ));
             new_node
@@ -213,19 +240,23 @@ fn inline_code_dfs(
             let new_node = new_graph.new_node(StatType::deleted());
             mappings.insert(node.clone(), new_node.clone());
             let true_node = inline_code_dfs(
+                new_call_nodes,
                 true_node,
                 mappings,
                 new_graph,
                 call_successor.clone(),
                 assigned_var,
+                new_variable,
                 variable_mappings,
             );
             let false_node = inline_code_dfs(
+                new_call_nodes,
                 false_node,
                 mappings,
                 new_graph,
                 call_successor,
                 assigned_var,
+                new_variable,
                 variable_mappings,
             );
             true_node.get().add_incoming(new_node.clone());
@@ -233,22 +264,27 @@ fn inline_code_dfs(
             let incoming = new_node.get().incoming();
             new_node.set(StatType::Branch(
                 incoming,
-                get_mapping(*var, variable_mappings),
+                get_mapping(*var, new_variable, variable_mappings),
                 true_node,
                 false_node,
             ));
             new_node
         }
-        StatType::Loop(_) => new_graph.new_node(StatType::new_loop()),
+        StatType::Loop(_) => {
+            let new_node = new_graph.new_node(StatType::new_loop());
+            mappings.insert(node.clone(), new_node.clone());
+            new_node
+        }
         StatType::Return(_, var) => {
             if let (Some(assigned_var), Some(var)) = (assigned_var, var) {
                 let new_node = new_graph.new_node(StatType::new_simple(
                     StatCode::Assign(
                         assigned_var,
-                        OpSrc::Var(get_mapping(*var, variable_mappings)),
+                        OpSrc::Var(get_mapping(*var, new_variable, variable_mappings)),
                     ),
                     call_successor,
                 ));
+                mappings.insert(node.clone(), new_node.clone());
                 call_successor.get().add_incoming(new_node.clone());
                 new_node
             } else {
@@ -259,13 +295,176 @@ fn inline_code_dfs(
     }
 }
 
+fn copy_code_dfs(
+    new_call_nodes: &mut LinkedList<StatNode>,
+    node: &StatNode,
+    mappings: &mut HashMap<StatNode, StatNode>,
+    new_graph: &mut Graph<StatType>,
+    new_variable: &mut VarRepr,
+    variable_mappings: &mut HashMap<VarRepr, VarRepr>,
+) -> (StatNode, f64) {
+    if mappings.contains_key(node) {
+        return (mappings[node].clone(), 0.0);
+    };
+    let node_instruction_heuristic = get_stat_size_heuristic(&*node.get());
+    match &*node.get() {
+        StatType::Simple(_, stat_code, next_node) => {
+            let new_node = new_graph.new_node(StatType::deleted());
+            mappings.insert(node.clone(), new_node.clone());
+            if get_call_name(node).is_some() {
+                new_call_nodes.push_back(new_node);
+            }
+            let (next_node, instruction_sum) = copy_code_dfs(
+                new_call_nodes,
+                next_node,
+                mappings,
+                new_graph,
+                new_variable,
+                variable_mappings,
+            );
+            next_node.get().add_incoming(new_node.clone());
+            let incoming = new_node.get().incoming();
+            new_node.set(StatType::Simple(
+                incoming,
+                substitute_vars(stat_code, new_variable, variable_mappings),
+                next_node,
+            ));
+            (new_node, instruction_sum + node_instruction_heuristic)
+        }
+        StatType::Branch(_, var, true_node, false_node) => {
+            let new_node = new_graph.new_node(StatType::deleted());
+            mappings.insert(node.clone(), new_node.clone());
+            let (true_node, true_instruction_sum) = copy_code_dfs(
+                new_call_nodes,
+                true_node,
+                mappings,
+                new_graph,
+                new_variable,
+                variable_mappings,
+            );
+            let (false_node, false_instruction_sum) = copy_code_dfs(
+                new_call_nodes,
+                false_node,
+                mappings,
+                new_graph,
+                new_variable,
+                variable_mappings,
+            );
+            true_node.get().add_incoming(new_node.clone());
+            false_node.get().add_incoming(new_node.clone());
+            let incoming = new_node.get().incoming();
+            new_node.set(StatType::Branch(
+                incoming,
+                get_mapping(*var, new_variable, variable_mappings),
+                true_node,
+                false_node,
+            ));
+            (
+                new_node,
+                true_instruction_sum + false_instruction_sum + node_instruction_heuristic,
+            )
+        }
+        StatType::Loop(_) => {
+            let new_node = new_graph.new_node(StatType::new_loop());
+            mappings.insert(node.clone(), new_node.clone());
+            (new_node, node_instruction_heuristic)
+        }
+        StatType::Return(_, var) => {
+            let new_node = new_graph.new_node(StatType::new_return(
+                var.map(|var| get_mapping(var, new_variable, variable_mappings)),
+            ));
+            mappings.insert(node.clone(), new_node.clone());
+            (new_node, node_instruction_heuristic)
+        }
+        StatType::Dummy(_) => panic!("Dummy nodes not expected at this point"),
+    }
+}
+
 fn inline_graph(
     code: StatNode,
     new_graph: &mut Graph<StatType>,
-    sccs: &HashMap<&str, HashSet<&str>>,
-    function_call_counts: &HashMap<&str, usize>,
-    variable_mappings: &mut HashMap<VarRepr, VarRepr>,
-) -> StatNode {
+    instructions_limit: usize,
+    args: &Vec<VarRepr>,
+    functions: &HashMap<&str, Function>,
+    function_instruction_counts: &HashMap<&str, usize>,
+    sccs_group: &HashSet<&str>,
+) -> (StatNode, Vec<VarRepr>) {
+    let mut call_nodes = LinkedList::new();
+    let mut main_variable_mappings = HashMap::new();
+    let (new_code, code_instruction_count) = copy_code_dfs(
+        &mut call_nodes,
+        &code,
+        &mut HashMap::new(),
+        new_graph,
+        &mut 0,
+        &mut main_variable_mappings,
+    );
+    let args = args.iter().map(|arg| main_variable_mappings[arg]).collect();
+    let code_instruction_count = code_instruction_count.ceil() as usize;
+    while let Some(call_node) = call_nodes.pop_front() {
+        let fname = get_call_name(&call_node).expect("Not a call node");
+        let function_instruction_count = function_instruction_counts[fname];
+        if sccs_group.contains(fname)
+            || code_instruction_count + function_instruction_count > instructions_limit
+        {
+            continue;
+        }
+        code_instruction_count += function_instruction_count;
+        let (incoming, assigned_var, call_successor, call_args) = match call_node
+            .set(StatType::deleted())
+        {
+            StatType::Simple(
+                incoming,
+                StatCode::Call(assigned_var, fname, call_args),
+                call_successor,
+            ) => (incoming, Some(assigned_var), call_successor, call_args),
+            StatType::Simple(incoming, StatCode::VoidCall(fname, call_args), call_successor) => {
+                (incoming, None, call_successor, call_args)
+            }
+            _ => panic!("Not a call node"),
+        };
+        let Function {
+            args: function_args,
+            code: function_code,
+            read_ref: _,
+        } = &functions[fname];
+        let new_call_node = function_code
+            .as_ref()
+            .map(|function_code| {
+                let mut variable_mappings =
+                    zip(call_args, function_args.iter().cloned()).collect::<HashMap<_, _>>();
+                let mut new_variable = variable_mappings.keys().cloned().max().unwrap_or_default();
+                inline_code_dfs(
+                    &mut call_nodes,
+                    function_code,
+                    &mut HashMap::new(),
+                    new_graph,
+                    call_successor,
+                    assigned_var,
+                    &mut new_variable,
+                    &mut variable_mappings,
+                )
+            })
+            .unwrap_or(call_successor);
+        call_node.set(new_call_node.set(StatType::deleted()));
+        for call_successor in call_node.get().successors() {
+            let successor_incoming = call_successor.get().incoming();
+            call_successor.get_mut().set_incoming(
+                successor_incoming
+                    .into_iter()
+                    .map(|successor_incoming_node| {
+                        if successor_incoming_node == new_call_node {
+                            call_node.clone()
+                        } else {
+                            successor_incoming_node
+                        }
+                    })
+                    .collect(),
+            );
+        }
+        new_graph.remove_node(new_call_node);
+    }
+    (new_code, args)
 }
 
 fn inline(
