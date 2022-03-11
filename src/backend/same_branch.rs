a48636222449e28ff83d756251a4f1e3c4ac5eca
barci2 <bcieslar2001@gmail.com>
2022-03-11 17:03:44 +0000

    Partial implementation of the optimization

--- src/backend/same_branch.rs
+++ src/backend/same_branch.rs
@@ -1,3 +1,7 @@
+use std::collections::{HashMap, LinkedList};
+
+use crate::graph::Deleted;
+
 use super::three_code::*;
 
 pub(super) fn same_branch_optimization(
@@ -10,5 +14,23 @@ pub(super) fn same_branch_optimization(
         int_handler: _,
     }: ThreeCode,
 ) -> ThreeCode {
-    todo!()
+    let mut removed_node_mapping = HashMap::new();
+    let needless_branches = LinkedList::new();
+    for node in &graph {
+        if let StatType::Branch(_, _, true_node, false_node) = &*node.get() && true_node == false_node {
+            needless_branches.push_back(node.clone());
+        }
+    }
+    while let Some(node) = needless_branches.pop_front() {
+        let (incoming, next_node) = if let StatType::Branch(incoming, _, true_node, false_node) =
+            node.set(StatType::deleted())
+        {
+            assert_eq!(true_node, false_node);
+            (incoming, true_node)
+        } else {
+            panic!("Expected a branch node")
+        };
+        removed_node_mapping.insert(node, next_node);
+    }
+    assert!(needless_branches.is_empty());
 }
