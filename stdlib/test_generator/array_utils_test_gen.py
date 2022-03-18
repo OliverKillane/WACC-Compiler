67315cd1a75c01e811ba1f1d2919e245f75b6cd4
Panayiotis Gavriil <panayiotis.gavriil19@imperial.ac.uk>
2022-03-18 11:22:50 +0000

    Reformatted test generator
    Signed-off-by: Panayiotis Gavriil <panayiotis.gavriil19@imperial.ac.uk>

--- stdlib/test_generator/array_utils_test_gen.py
+++ stdlib/test_generator/array_utils_test_gen.py
@@ -4,7 +4,14 @@ from sys import exit
 from random import getrandbits, randint, choice
 
 ##### DATA TO CHOOSE FROM DEPENDING ON THE TYPE #####
-rand_per_type = {"int": "randint(-2147483648, 2147483647)", "bool": "str(bool(getrandbits(1))).lower()", "char": "chr(choice(list(set(range(32, 127)) - set([34, 35, 39, 92]))))", "short_int": "randint(-3, 3)", "positive_short_int": "randint(1, 3)", "short_char": "chr(choice(list(set(range(88, 100)) - set([92]))))"}
+rand_per_type = {
+    "int": "randint(-2147483648, 2147483647)",
+    "bool": "str(bool(getrandbits(1))).lower()",
+    "char": "chr(choice(list(set(range(32, 127)) - set([34, 35, 39, 92]))))",
+    "short_int": "randint(-3, 3)",
+    "positive_short_int": "randint(1, 3)",
+    "short_char": "chr(choice(list(set(range(88, 100)) - set([92]))))",
+}
 
 ##### FUNCTION TO GENERATE PRINT TESTS #####
 def print_arr(function_name, type, no_of_test_cases, file):
@@ -22,12 +29,14 @@ def print_arr(function_name, type, no_of_test_cases, file):
     for arr in arrs:
         file.write(f"# arr = {arr}\n")
 
-    
     path = "../../../../../stdlib/array_utils.wacc"
-    file.write(f"\n\nmod {path};\n\nbegin\n\t{type}[] arr = {arrs[0]} ;\n\tcall {function_name}(\"arr\", arr) ;\n")
+    file.write(
+        f'\n\nmod {path};\n\nbegin\n\t{type}[] arr = {arrs[0]} ;\n\tcall {function_name}("arr", arr) ;\n'
+    )
 
     for i in range(1, no_of_test_cases):
-        file.write(f"\tarr = {arrs[i]} ;\n\tcall {function_name}(\"arr\", arr) ;\n")
+        file.write(f'\tarr = {arrs[i]} ;\n\tcall {function_name}("arr", arr) ;\n')
+
 
 ##### FUNCTION TO GENERATE COUNT TESTS #####
 def count_arr(function_name, type, no_of_test_cases, file):
@@ -48,15 +57,19 @@ def count_arr(function_name, type, no_of_test_cases, file):
     for i in range(no_of_test_cases):
         file.write(f"# {countOf(arrs[i], to_count[i])}\n")
 
-    
     path = "../../../../../stdlib/array_utils.wacc"
 
     padding = "'" if type == "char" else ""
 
-    file.write(f"\n\nmod {path};\n\nbegin\n\t{type}[] arr = {arrs[0]} ;\n\tint count = call count_{type}_arr({padding}{to_count[0]}{padding}, arr) ;\n\tprintln count ;\n")
+    file.write(
+        f"\n\nmod {path};\n\nbegin\n\t{type}[] arr = {arrs[0]} ;\n\tint count = call count_{type}_arr({padding}{to_count[0]}{padding}, arr) ;\n\tprintln count ;\n"
+    )
 
     for i in range(1, no_of_test_cases):
-            file.write(f"\tarr = {arrs[i]} ;\n\tcount = call count_{type}_arr({padding}{to_count[i]}{padding}, arr) ;\n\tprintln count ;\n")
+        file.write(
+            f"\tarr = {arrs[i]} ;\n\tcount = call count_{type}_arr({padding}{to_count[i]}{padding}, arr) ;\n\tprintln count ;\n"
+        )
+
 
 ##### FUNCTION TO GENERATE FILL TESTS #####
 def fill_arr(function_name, type, no_of_testcases, file):
@@ -82,21 +95,32 @@ def fill_arr(function_name, type, no_of_testcases, file):
 
     padding = "'" if type == "char" else ""
 
-    file.write(f"\n\nmod {path};\n\nbegin\n\t{type}[] arr = {arrs[0]} ;\n\tcall print_{type}_arr(\"arr_before\", arr) ;\n\tcall {function_name}({padding}{to_fill_with[0]}{padding}, arr) ;\n\tcall print_{type}_arr(\"arr_after\", arr) ;\n")
+    file.write(
+        f'\n\nmod {path};\n\nbegin\n\t{type}[] arr = {arrs[0]} ;\n\tcall print_{type}_arr("arr_before", arr) ;\n\tcall {function_name}({padding}{to_fill_with[0]}{padding}, arr) ;\n\tcall print_{type}_arr("arr_after", arr) ;\n'
+    )
 
     for i in range(1, no_of_testcases):
-        file.write(f"\tarr = {arrs[i]} ;\n\tcall print_{type}_arr(\"arr_before\", arr) ;\n\tcall {function_name}({padding}{to_fill_with[i]}{padding}, arr) ;\n\tcall print_{type}_arr(\"arr_after\", arr) ;\n")
+        file.write(
+            f'\tarr = {arrs[i]} ;\n\tcall print_{type}_arr("arr_before", arr) ;\n\tcall {function_name}({padding}{to_fill_with[i]}{padding}, arr) ;\n\tcall print_{type}_arr("arr_after", arr) ;\n'
+        )
+
 
 ##### DICTIONARY OF AVAILABLE FUNCTIONS TO GENERATE TESTS FOR AND WHAT TYPE OF TESTS CAN BE GENEREATED FOR THOSE FUNCTIONS #####
 function_dict = {"print_arr": print_arr, "count_arr": count_arr, "fill_arr": fill_arr}
-type_per_function = {"print_arr": ["int", "bool", "char"], "count_arr": ["int", "bool", "char"], "fill_arr": ["int", "bool", "char"]}
+type_per_function = {
+    "print_arr": ["int", "bool", "char"],
+    "count_arr": ["int", "bool", "char"],
+    "fill_arr": ["int", "bool", "char"],
+}
 
-
+##### MAIN FUNCTION #####
 if __name__ == "__main__":
 
     n = int(input("How many functions would you like to test?\n\n"))
 
-    print("\nWhat test(s) and of what type(s) would you like?\nGive your choice(s) as 'function_with_type_placeholder type1,type2'\ne.g. 'print_type_arr int,bool' or 'print_type_arr all'\n")
+    print(
+        "\nWhat test(s) and of what type(s) would you like?\nGive your choice(s) as 'function_with_type_placeholder type1,type2'\ne.g. 'print_type_arr int,bool' or 'print_type_arr all'\n"
+    )
 
     tests_types = dict(input("").split() for _ in range(n))
 
@@ -108,28 +132,32 @@ if __name__ == "__main__":
         function_name = test.replace("_type_", "_")
 
         if function_name not in function_dict:
-                exit(f"The given function ('{function_name}') wasn't found in the function dictionary!")
-    
+            exit(
+                f"The given function ('{function_name}') wasn't found in the function dictionary!"
+            )
+
         if type == "all":
             selected_types = type_per_function[function_name]
         else:
             selected_types = type.split(",")
-        
+
         for sel_type in selected_types:
             if sel_type not in type_per_function[function_name]:
                 exit(f"Invalid type ('{sel_type}') given!")
 
             local_function_name = test.replace("_type_", f"_{sel_type}_")
-            file = open(local_function_name + ".wacc", 'w+')
+            file = open(local_function_name + ".wacc", "w+")
 
-            function_dict[function_name](local_function_name, sel_type, no_of_test_cases, file)
+            function_dict[function_name](
+                local_function_name, sel_type, no_of_test_cases, file
+            )
 
             file.seek(file.tell() - 3, os.SEEK_SET)
             file.write("\nend")
 
             if sel_type == "bool":
                 file.seek(0)
-                to_write_back = file.read().replace("\'", "")
+                to_write_back = file.read().replace("'", "")
                 file.seek(0)
                 file.truncate()
                 file.write(to_write_back)
